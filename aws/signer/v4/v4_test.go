c49645297497379eb6218e44a07979c6999be4ba
euank <euank@euank.com>
2015-02-05 02:04:24 -0800

    Refactor signer to work for Reader
    See #81 for discussion

--- aws/signer/v4/v4_test.go
+++ aws/signer/v4/v4_test.go
@@ -1,6 +1,10 @@
 package v4
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"strings"
 	"testing"
@@ -22,7 +26,6 @@ func buildSigner(serviceName string, region string, signTime time.Time, body str
 		Time:            signTime,
 		ExpireTime:      300 * time.Second,
 		Query:           req.URL.Query(),
-		Body:            reader,
 		ServiceName:     serviceName,
 		Region:          region,
 		AccessKeyID:     "AKID",
@@ -64,5 +67,62 @@ func BenchmarkSignRequest(b *testing.B) {
 	signer := buildSigner("dynamodb", "us-east-1", time.Now(), "{}")
 	for i := 0; i < b.N; i++ {
 		signer.sign()
+	}
+}
+
+type readSeekCloser struct {
+	*bytes.Reader
+}
+
+func (r readSeekCloser) Close() error {
+	return nil
+}
+
+func TestMakeShaReaderReadSeeker(t *testing.T) {
+	var seeker io.ReadSeeker
+	testData := []byte(`I am a read seeker`)
+	seeker = &readSeekCloser{bytes.NewReader(testData)}
+	mockRequest, _ := http.NewRequest("GET", "http://example.com", seeker)
+
+	checksum := makeSha256Reader(mockRequest)
+	expectedCheckum := sha256.Sum256(testData)
+
+	if !bytes.Equal(checksum, expectedCheckum[:]) {
+		t.Errorf("Checksum did not match expected; was %v instead of %v", checksum, sha256.Sum256(testData))
+	}
+
+	// Verify it was rewound
+	data, err := ioutil.ReadAll(mockRequest.Body)
+	if err != nil {
+		t.Errorf("Unable to read seeker: %v", err)
+	}
+	if !bytes.Equal(data, testData) {
+		t.Errorf("Re-reading reader got different result: %v != %v", string(data), string(testData))
+	}
+}
+
+func TestMakeShaReaderNotSeeker(t *testing.T) {
+	var reader io.Reader
+	testData := []byte(`I am not a read seeker`)
+	reader = bytes.NewBuffer(testData)
+	if _, ok := reader.(io.ReadSeeker); ok {
+		t.Fatal("We're trying to test something that's not a readseeker here. Fix the test or downgrade Go :)")
+	}
+	mockRequest, _ := http.NewRequest("GET", "http://example.com", reader)
+
+	checksum := makeSha256Reader(mockRequest)
+	expectedCheckum := sha256.Sum256(testData)
+
+	if !bytes.Equal(checksum, expectedCheckum[:]) {
+		t.Errorf("Checksum did not match expected; was %v instead of %v", checksum, sha256.Sum256(testData))
+	}
+
+	// Verify it was rewound
+	data, err := ioutil.ReadAll(mockRequest.Body)
+	if err != nil {
+		t.Errorf("Unable to read seeker: %v", err)
+	}
+	if !bytes.Equal(data, testData) {
+		t.Errorf("Re-reading reader got different result: %v != %v", string(data), string(testData))
 	}
 }
