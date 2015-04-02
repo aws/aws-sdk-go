61d528d01aeb5650d1a3b8a491ec0e3a2e2d07c6
Loren Segal <lsegal@amazon.com>
2015-04-01 17:08:47 -0700

    Add awsutil.Copy() and awsutil.CopyOf() to generate deep copies of request/response structures

---
+++ aws/awsutil/copy.go
@@ -0,0 +1,57 @@
+package awsutil
+
+import "reflect"
+
+// Copy deeply copies a src structure to dst. Useful for copying request and
+// response structures.
+func Copy(dst, src interface{}) {
+	rcopy(reflect.ValueOf(dst), reflect.ValueOf(src))
+}
+
+// CopyOf returns a copy of src while also allocating the memory for dst.
+// src must be a pointer type or this operation will fail.
+func CopyOf(src interface{}) (dst interface{}) {
+	dsti := reflect.New(reflect.TypeOf(src).Elem())
+	dst = dsti.Interface()
+	rcopy(dsti, reflect.ValueOf(src))
+	return
+}
+
+func rcopy(dst, src reflect.Value) {
+	if !src.IsValid() {
+		return
+	}
+
+	switch src.Kind() {
+	case reflect.Ptr:
+		e := src.Type().Elem()
+		if dst.CanSet() {
+			dst.Set(reflect.New(e))
+		}
+		if src.Elem().IsValid() {
+			rcopy(dst.Elem(), src.Elem())
+		}
+	case reflect.Struct:
+		dst.Set(reflect.New(src.Type()).Elem())
+		for i := 0; i < src.NumField(); i++ {
+			rcopy(dst.Field(i), src.Field(i))
+		}
+	case reflect.Slice:
+		s := reflect.MakeSlice(src.Type(), src.Len(), src.Cap())
+		dst.Set(s)
+		for i := 0; i < src.Len(); i++ {
+			rcopy(dst.Index(i), src.Index(i))
+		}
+	case reflect.Map:
+		s := reflect.MakeMap(src.Type())
+		dst.Set(s)
+		for _, k := range src.MapKeys() {
+			v := src.MapIndex(k)
+			v2 := reflect.New(v.Type()).Elem()
+			rcopy(v2, v)
+			dst.SetMapIndex(k, v2)
+		}
+	default:
+		dst.Set(src)
+	}
+}
