c55b0f3e3a0f2288dd6b7c0976854b085b811050
Jason Del Ponte <delpontej@gmail.com>
2015-06-03 13:40:55 -0700

    Updating package description

--- sdk.go
+++ sdk.go
@@ -1,2 +1,8 @@
-// Package sdk is a stub package for go get not to report erroneous errors. See README.md for usage information.
+// Package sdk is the official AWS SDK for the Go programming language.
+//
+// See our Developer Guide for information for on getting started and using
+// the SDK.
+//
+// https://github.com/aws/aws-sdk-go/wiki
 package sdk
+
