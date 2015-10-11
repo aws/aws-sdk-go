bf9430e015b2da0e8c91e3bbeb8c8a8f46de2ff2
Gabe Rosenhouse <grosenhouse@pivotal.io>
2015-10-11 12:54:54 -0700

    Strongly-typed CloudFormation templates
    - first pass, no tests or docs
    - https://github.com/aws/aws-sdk-go/issues/393

---
+++ service/cloudformation/template/template.go
@@ -0,0 +1,51 @@
+package cloudformation
+
+import "encoding/json"
+
+// http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/template-anatomy.html
+type Template struct {
+	AWSTemplateFormatVersion string                 `json:",omitempty"`
+	Description              string                 `json:",omitempty"`
+	Metadata                 map[string]interface{} `json:",omitempty"`
+	Parameters               map[string]Parameter   `json:",omitempty"`
+	Mappings                 map[string]Mapping     `json:",omitempty"`
+	Conditions               map[string]Condition   `json:",omitempty"`
+	Resources                map[string]Resource    `json:",omitempty"`
+	Outputs                  map[string]Output      `json:",omitempty"`
+}
+
+type Parameter struct {
+	Type                  string
+	Default               string   `json:",omitempty"`
+	NoEcho                bool     `json:",omitempty,string"`
+	AllowedValues         []string `json:",omitempty"`
+	AllowedPattern        string   `json:",omitempty"`
+	MaxLength             int      `json:",omitempty,string"`
+	MinLength             int      `json:",omitempty,string"`
+	Description           string   `json:",omitempty"`
+	ConstraintDescription string   `json:",omitempty"`
+}
+
+type Mapping map[string]map[string]string
+
+type Condition map[string]interface{}
+
+type Resource struct {
+	Type       string
+	Properties map[string]interface{}
+}
+
+type Output struct {
+	Description string `json:",omitempty"`
+	Value       string
+}
+
+type Ref struct{ Ref string }
+
+func (t *Template) String() string {
+	bytes, err := json.Marshal(t)
+	if err != nil {
+		panic(err)
+	}
+	return string(bytes)
+}
