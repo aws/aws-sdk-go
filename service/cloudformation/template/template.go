0d19d640a1e8868a8a61c037bf83013cacd7391f
Gabe Rosenhouse <grosenhouse@pivotal.io>
2015-10-11 16:00:50 -0700

    Adds FnGetAtt, FnGetAZs, FnSelect CloudFormation functions

--- service/cloudformation/template/template.go
+++ service/cloudformation/template/template.go
@@ -1,6 +1,9 @@
 package template
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 // http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/template-anatomy.html
 type Template struct {
@@ -63,6 +66,31 @@ func FnFindInMap(mapName string, topLevelKey interface{}, secondLevelKey interfa
 	return struct {
 		Keys []interface{} `json:"Fn::FindInMap"`
 	}{[]interface{}{mapName, topLevelKey, secondLevelKey}}
+}
+
+func FnGetAtt(logicalNameOfResource string, attributeName interface{}) interface{} {
+	return struct {
+		Att []interface{} `json:"Fn::GetAtt"`
+	}{[]interface{}{logicalNameOfResource, attributeName}}
+}
+
+func FnGetAZs(region interface{}) interface{} {
+	if region == nil {
+		region = ""
+	}
+	return struct {
+		AZs interface{} `json:"Fn::GetAZs"`
+	}{region}
+}
+
+func FnSelect(index interface{}, listOfObjects interface{}) interface{} {
+	switch intIndex := index.(type) {
+	case int:
+		index = fmt.Sprintf("%d", intIndex)
+	}
+	return struct {
+		Select []interface{} `json:"Fn::Select"`
+	}{[]interface{}{index, listOfObjects}}
 }
 
 func FnJoin(delimeter interface{}, listOfValues ...interface{}) interface{} {
--- service/cloudformation/template/template_test.go
+++ service/cloudformation/template/template_test.go
@@ -18,58 +18,62 @@ func TestParameterStringify(t *testing.T) {
 		MaxLength: 22,
 		MinLength: 2,
 	}
-
-	actual, err := json.Marshal(param)
-	assert.Nil(t, err)
-
-	expected := `{ "Type": "some-parameter", "NoEcho": "true", "MinLength": "2", "MaxLength": "22" }`
-
-	assertEquivalentJSON(t, expected, string(actual))
-}
-
-func TestRefs(t *testing.T) {
-	ref := Ref("AWS::StackId")
-
-	actual, err := json.Marshal(ref)
-	assert.Nil(t, err)
-
-	assertEquivalentJSON(t, `{"Ref":"AWS::StackId" }`, string(actual))
+	assertMarshalsTo(t, param, `{ "Type": "some-parameter", "NoEcho": "true", "MinLength": "2", "MaxLength": "22" }`)
 }
 
 func TestFnBase64(t *testing.T) {
 	base64 := FnBase64("some string to encode")
-
-	actual, err := json.Marshal(base64)
-	assert.Nil(t, err)
-
-	assertEquivalentJSON(t, `{ "Fn::Base64" : "some string to encode" }`, string(actual))
+	assertMarshalsTo(t, base64, `{ "Fn::Base64" : "some string to encode" }`)
 }
 
 func TestFnBase64WithObjects(t *testing.T) {
 	base64 := FnBase64(Ref("some-logical-name"))
-
-	actual, err := json.Marshal(base64)
-	assert.Nil(t, err)
-
-	assertEquivalentJSON(t, `{ "Fn::Base64" : { "Ref": "some-logical-name" } }`, string(actual))
+	assertMarshalsTo(t, base64, `{ "Fn::Base64" : { "Ref": "some-logical-name" } }`)
 }
 
 func TestFnFindInMap(t *testing.T) {
 	mapped := FnFindInMap("RegionMap", Ref("AWS::Region"), "32")
+	assertMarshalsTo(t, mapped, `{ "Fn::FindInMap" : [ "RegionMap", { "Ref" : "AWS::Region" }, "32"]}`)
+}
 
-	actual, err := json.Marshal(mapped)
-	assert.Nil(t, err)
+func TestFnGetAtt(t *testing.T) {
+	att := FnGetAtt("MyLB", Ref("something"))
+	assertMarshalsTo(t, att, `{ "Fn::GetAtt": [ "MyLB" , { "Ref": "something" } ] }`)
+}
 
-	assertEquivalentJSON(t, `{ "Fn::FindInMap" : [ "RegionMap", { "Ref" : "AWS::Region" }, "32"]}`, string(actual))
+func TestFnGetAZs(t *testing.T) {
+	azs := FnGetAZs(Ref("AWS::Region"))
+	assertMarshalsTo(t, azs, `{ "Fn::GetAZs": { "Ref": "AWS::Region" } }`)
+}
+
+func TestFnGetAZsEmpty(t *testing.T) {
+	azs := FnGetAZs("")
+	assertMarshalsTo(t, azs, `{ "Fn::GetAZs": "" }`)
+}
+
+func TestFnGetAZsNil(t *testing.T) {
+	azs := FnGetAZs(nil)
+	assertMarshalsTo(t, azs, `{ "Fn::GetAZs": "" }`)
 }
 
 func TestFnJoin(t *testing.T) {
 	joined := FnJoin("\n", "some", "list", Ref("AWS::StackId"))
+	assertMarshalsTo(t, joined, `{ "Fn::Join" : ["\n", [ "some", "list", { "Ref" : "AWS::StackId" } ] ] }`)
+}
 
-	actual, err := json.Marshal(joined)
-	assert.Nil(t, err)
+func TestFnSelect(t *testing.T) {
+	selected := FnSelect(Ref("MyIndex"), FnGetAZs(""))
+	assertMarshalsTo(t, selected, `{ "Fn::Select" : [ { "Ref": "MyIndex" }, { "Fn::GetAZs": "" } ] }`)
+}
 
-	assertEquivalentJSON(t, `{ "Fn::Join" : ["\n", [ "some", "list", { "Ref" : "AWS::StackId" } ] ] }`, string(actual))
+func TestFnSelectWithLiteralIndex(t *testing.T) {
+	selected := FnSelect(2, FnGetAZs(""))
+	assertMarshalsTo(t, selected, `{ "Fn::Select" : [ "2", { "Fn::GetAZs": "" } ] }`)
+}
+
+func TestRefs(t *testing.T) {
+	ref := Ref("AWS::StackId")
+	assertMarshalsTo(t, ref, `{"Ref":"AWS::StackId" }`)
 }
 
 func TestTemplateCreation(t *testing.T) {
@@ -106,8 +110,6 @@ func TestTemplateCreation(t *testing.T) {
 		},
 	}
 
-	actual := template.String()
-
 	expected := `
 {
   "AWSTemplateFormatVersion" : "2010-09-09",
@@ -141,7 +143,17 @@ func TestTemplateCreation(t *testing.T) {
   }
 }
 `
-	assertEquivalentJSON(t, expected, actual)
+	assertMarshalsTo(t, template, expected)
+
+	asString := template.String()
+	assertEquivalentJSON(t, expected, asString)
+}
+
+func assertMarshalsTo(t *testing.T, value interface{}, expectedJSON string) {
+	actual, err := json.Marshal(value)
+	assert.Nil(t, err)
+
+	assertEquivalentJSON(t, expectedJSON, string(actual))
 }
 
 func assertEquivalentJSON(t *testing.T, expected, actual string) {
