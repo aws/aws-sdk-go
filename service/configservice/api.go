e334d8e52e4800cefa3038facd39011db07aeb81
Jason Del Ponte <delpontej@gmail.com>
2015-12-17 12:25:31 -0800

    service/configservice: Add support for IAM resource types
    AWS Config

--- service/configservice/api.go
+++ service/configservice/api.go
@@ -1087,15 +1087,31 @@ type ConfigRuleEvaluationStatus struct {
 	// The time that you first activated the AWS Config rule.
 	FirstActivatedTime *time.Time `type:"timestamp" timestampFormat:"unix"`
 
+	// Indicates whether AWS Config has evaluated your resources against the rule
+	// at least once.
+	//
+	//  true - AWS Config has evaluated your AWS resources against the rule at
+	// least once. false - AWS Config has not once finished evaluating your AWS
+	// resources against the rule.
+	FirstEvaluationStarted *bool `type:"boolean"`
+
 	// The error code that AWS Config returned when the rule last failed.
 	LastErrorCode *string `type:"string"`
 
 	// The error message that AWS Config returned when the rule last failed.
 	LastErrorMessage *string `type:"string"`
 
+	// The time that AWS Config last failed to evaluate your AWS resources against
+	// the rule.
+	LastFailedEvaluationTime *time.Time `type:"timestamp" timestampFormat:"unix"`
+
 	// The time that AWS Config last failed to invoke the AWS Config rule to evaluate
 	// your AWS resources.
 	LastFailedInvocationTime *time.Time `type:"timestamp" timestampFormat:"unix"`
+
+	// The time that AWS Config last successfully evaluated your AWS resources against
+	// the rule.
+	LastSuccessfulEvaluationTime *time.Time `type:"timestamp" timestampFormat:"unix"`
 
 	// The time that AWS Config last successfully invoked the AWS Config rule to
 	// evaluate your AWS resources.
@@ -1254,9 +1270,8 @@ type ConfigurationRecorder struct {
 	// the assigned name.
 	Name *string `locationName:"name" min:"1" type:"string"`
 
-	// The recording group specifies either to record configurations for all supported
-	// resources or to provide a list of resource types to record. The list of resource
-	// types must be a subset of supported resource types.
+	// Specifies the types of AWS resource for which AWS Config records configuration
+	// changes.
 	RecordingGroup *RecordingGroup `locationName:"recordingGroup" type:"structure"`
 
 	// Amazon Resource Name (ARN) of the IAM role used to describe the AWS resources
@@ -2378,24 +2393,71 @@ func (s PutEvaluationsOutput) GoString() string {
 	return s.String()
 }
 
-// The group of AWS resource types that AWS Config records when starting the
-// configuration recorder.
+// Specifies the types of AWS resource for which AWS Config records configuration
+// changes.
 //
-// recordingGroup can have one and only one parameter. Choose either allSupported
-// or resourceTypes.
+// In the recording group, you specify whether all supported types or specific
+// types of resources are recorded.
+//
+// By default, AWS Config records configuration changes for all supported types
+// of regional resources that AWS Config discovers in the region in which it
+// is running. Regional resources are tied to a region and can be used only
+// in that region. Examples of regional resources are EC2 instances and EBS
+// volumes.
+//
+// You can also have AWS Config record configuration changes for supported
+// types of global resources. Global resources are not tied to an individual
+// region and can be used in all regions.
+//
+// The configuration details for any global resource are the same in all regions.
+// If you customize AWS Config in multiple regions to record global resources,
+// it will create multiple configuration items each time a global resource changes:
+// one configuration item for each region. These configuration items will contain
+// identical data. To prevent duplicate configuration items, you should consider
+// customizing AWS Config in only one region to record global resources, unless
+// you want the configuration items to be available in multiple regions. If
+// you don't want AWS Config to record all resources, you can specify which
+// types of resources it will record with the resourceTypes parameter.
+//
+// For a list of supported resource types, see Supported resource types (http://docs.aws.amazon.com/config/latest/developerguide/resource-config-reference.html#supported-resources).
+//
+// For more information, see Selecting Which Resources AWS Config Records (http://docs.aws.amazon.com/config/latest/developerguide/select-resources.html).
 type RecordingGroup struct {
 	_ struct{} `type:"structure"`
 
-	// Records all supported resource types in the recording group. For a list of
-	// supported resource types, see Supported resource types (http://docs.aws.amazon.com/config/latest/developerguide/resource-config-reference.html#supported-resources).
-	// If you specify allSupported, you cannot enumerate a list of resourceTypes.
+	// Specifies whether AWS Config records configuration changes for every supported
+	// type of regional resource.
+	//
+	// If you set this option to true, when AWS Config adds support for a new type
+	// of regional resource, it automatically starts recording resources of that
+	// type.
+	//
+	// If you set this option to true, you cannot enumerate a list of resourceTypes.
 	AllSupported *bool `locationName:"allSupported" type:"boolean"`
 
-	// A comma-separated list of strings representing valid AWS resource types (for
-	// example, AWS::EC2::Instance or AWS::CloudTrail::Trail). resourceTypes is
-	// only valid if you have chosen not to select allSupported. For a list of valid
-	// resourceTypes values, see the resourceType Value column in the following
-	// topic: Supported AWS Resource Types (http://docs.aws.amazon.com/config/latest/developerguide/resource-config-reference.html#supported-resources).
+	// Specifies whether AWS Config includes all supported types of global resources
+	// with the resources that it records.
+	//
+	// Before you can set this option to true, you must set the allSupported option
+	// to true.
+	//
+	// If you set this option to true, when AWS Config adds support for a new type
+	// of global resource, it automatically starts recording resources of that type.
+	IncludeGlobalResourceTypes *bool `locationName:"includeGlobalResourceTypes" type:"boolean"`
+
+	// A comma-separated list that specifies the types of AWS resources for which
+	// AWS Config records configuration changes (for example, AWS::EC2::Instance
+	// or AWS::CloudTrail::Trail).
+	//
+	// Before you can set this option to true, you must set the allSupported option
+	// to false.
+	//
+	// If you set this option to true, when AWS Config adds support for a new type
+	// of resource, it will not record resources of that type unless you manually
+	// add that type to your recording group.
+	//
+	// For a list of valid resourceTypes values, see the resourceType Value column
+	// in Supported AWS Resource Types (http://docs.aws.amazon.com/config/latest/developerguide/resource-config-reference.html#supported-resources).
 	ResourceTypes []*string `locationName:"resourceTypes" type:"list"`
 }
 
@@ -2743,4 +2805,12 @@ const (
 	ResourceTypeAwsEc2Vpnconnection = "AWS::EC2::VPNConnection"
 	// @enum ResourceType
 	ResourceTypeAwsEc2Vpngateway = "AWS::EC2::VPNGateway"
+	// @enum ResourceType
+	ResourceTypeAwsIamGroup = "AWS::IAM::Group"
+	// @enum ResourceType
+	ResourceTypeAwsIamPolicy = "AWS::IAM::Policy"
+	// @enum ResourceType
+	ResourceTypeAwsIamRole = "AWS::IAM::Role"
+	// @enum ResourceType
+	ResourceTypeAwsIamUser = "AWS::IAM::User"
 )
