b588fce9ce7d4b0febcb8e928e64f24090c63d4f
xibz <impactbchang@gmail.com>
2016-01-28 11:56:47 -0800

    service/ssm: Add support for EC2 instance id length increase
    Amazon Simple Systems Management Service
    
    Add paginators and remove of instance id length constraint

--- service/ssm/ssmiface/interface.go
+++ service/ssm/ssmiface/interface.go
@@ -54,17 +54,25 @@ type SSMAPI interface {
 
 	ListAssociations(*ssm.ListAssociationsInput) (*ssm.ListAssociationsOutput, error)
 
+	ListAssociationsPages(*ssm.ListAssociationsInput, func(*ssm.ListAssociationsOutput, bool) bool) error
+
 	ListCommandInvocationsRequest(*ssm.ListCommandInvocationsInput) (*request.Request, *ssm.ListCommandInvocationsOutput)
 
 	ListCommandInvocations(*ssm.ListCommandInvocationsInput) (*ssm.ListCommandInvocationsOutput, error)
+
+	ListCommandInvocationsPages(*ssm.ListCommandInvocationsInput, func(*ssm.ListCommandInvocationsOutput, bool) bool) error
 
 	ListCommandsRequest(*ssm.ListCommandsInput) (*request.Request, *ssm.ListCommandsOutput)
 
 	ListCommands(*ssm.ListCommandsInput) (*ssm.ListCommandsOutput, error)
 
+	ListCommandsPages(*ssm.ListCommandsInput, func(*ssm.ListCommandsOutput, bool) bool) error
+
 	ListDocumentsRequest(*ssm.ListDocumentsInput) (*request.Request, *ssm.ListDocumentsOutput)
 
 	ListDocuments(*ssm.ListDocumentsInput) (*ssm.ListDocumentsOutput, error)
+
+	ListDocumentsPages(*ssm.ListDocumentsInput, func(*ssm.ListDocumentsOutput, bool) bool) error
 
 	SendCommandRequest(*ssm.SendCommandInput) (*request.Request, *ssm.SendCommandOutput)
 
