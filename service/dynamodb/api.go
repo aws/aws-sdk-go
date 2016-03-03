b391863c0f6261f7babe504c1b1e15cc676445cb
Jason Del Ponte <delpontej@gmail.com>
2016-03-03 14:08:04 -0800

    service/dynamodb: Update documentation

--- service/dynamodb/api.go
+++ service/dynamodb/api.go
@@ -340,32 +340,53 @@ func (c *DynamoDB) DescribeLimitsRequest(input *DescribeLimitsInput) (req *reque
 }
 
 // Returns the current provisioned-capacity limits for your AWS account in a
-// region, both for the region as a whole and for any one DynamoDB table you
-// create there.
+// region, both for the region as a whole and for any one DynamoDB table that
+// you create there.
 //
 // When you establish an AWS account, the account has initial limits on the
-// maximum Read- and Write-Capacity Units (RCUs and WCUs) that you can provision
-// across all your DynamoDB tables in a given region, and also per-table limits
-// that apply when you create a table there. Although you can increase these
-// limits by filing a Case at AWS Support Center (https://console.aws.amazon.com/support),
-// obtaining the increase is not instantaneous. The DescribeLimits API lets
-// you write code to compare the capacity you are currently using to those limits
-// imposed by your account so you have enough time to apply for an increase
-// before you hit a limit. For example, you could use one of the AWS SDKs to
-// do the following:
+// maximum read capacity units and write capacity units that you can provision
+// across all of your DynamoDB tables in a given region. Also, there are per-table
+// limits that apply when you create a table there. For more information, see
+// Limits (http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Limits.html)
+// page in the Amazon DynamoDB Developer Guide.
 //
-//  Call DescribeLimits to obtain the current account limits on provisioned
-// capacity in a region. Call ListTables to obtain a list of all your DynamoDB
-// tables there. Loop over the tables, calling DescribeTable on each. Sum the
-// capacity provisioned for each table and all its global secondary indexes
-// across all the tables, while also keeping track of the table or global secondary
-// index with the largest provisioned read capacity, and the one with the largest
-// provisioned write capacity. Report the account limits returned by DescribeLimits
-// as compared to the current provisioned capacity levels for the account as
-// a whole, and for the highest current per-table provisioning.  This will let
-// you see whether you are getting close to your account-level limits. See Limits
-// in DynamoDB (http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Limits.html#d0e53086)
-// for a code example written in Python.
+// Although you can increase these limits by filing a case at AWS Support Center
+// (https://console.aws.amazon.com/support/home#/), obtaining the increase is
+// not instantaneous. The DescribeLimits API lets you write code to compare
+// the capacity you are currently using to those limits imposed by your account
+// so that you have enough time to apply for an increase before you hit a limit.
+//
+//  For example, you could use one of the AWS SDKs to do the following:
+//
+//  Call DescribeLimits for a particular region to obtain your current account
+// limits on provisioned capacity there. Create a variable to hold the aggregate
+// read capacity units provisioned for all your tables in that region, and one
+// to hold the aggregate write capacity units. Zero them both. Call ListTables
+// to obtain a list of all your DynamoDB tables. For each table name listed
+// by ListTables, do the following:
+//
+//  Call DescribeTable with the table name. Use the data returned by DescribeTable
+// to add the read capacity units and write capacity units provisioned for the
+// table itself to your variables. If the table has one or more global secondary
+// indexes (GSIs), loop over these GSIs and add their provisioned capacity values
+// to your variables as well.   Report the account limits for that region returned
+// by DescribeLimits, along with the total current provisioned capacity levels
+// you have calculated.  This will let you see whether you are getting close
+// to your account-level limits.
+//
+// The per-table limits apply only when you are creating a new table. They
+// restrict the sum of the provisioned capacity of the new table itself and
+// all its global secondary indexes.
+//
+// For existing tables and their GSIs, DynamoDB will not let you increase provisioned
+// capacity extremely rapidly, but the only upper limit that applies is that
+// the aggregate provisioned capacity over all your tables and GSIs cannot exceed
+// either of the per-account limits.
+//
+// DescribeLimits should only be called periodically. You can expect throttling
+// errors if you call it more than once in a minute.
+//
+//  The DescribeLimits Request element has no content.
 func (c *DynamoDB) DescribeLimits(input *DescribeLimitsInput) (*DescribeLimitsOutput, error) {
 	req, out := c.DescribeLimitsRequest(input)
 	err := req.Send()
@@ -2147,22 +2168,22 @@ func (s DescribeLimitsInput) GoString() string {
 type DescribeLimitsOutput struct {
 	_ struct{} `type:"structure"`
 
-	// The maximum total Read-Capacity Units (RCUs) that your account allows you
-	// to provision across all your tables in this region.
+	// The maximum total read capacity units that your account allows you to provision
+	// across all of your tables in this region.
 	AccountMaxReadCapacityUnits *int64 `min:"1" type:"long"`
 
-	// The maximum total Write-Capacity Units (WCUs) that your account allows you
-	// to provision across all your tables in this region.
+	// The maximum total write capacity units that your account allows you to provision
+	// across all of your tables in this region.
 	AccountMaxWriteCapacityUnits *int64 `min:"1" type:"long"`
 
-	// The maximum Read-Capacity Units (RCUs) that your account allows you to provision
-	// for a new table you are creating in this region, including the RCUs provisioned
-	// for its global secondary indexes (GSIs).
+	// The maximum read capacity units that your account allows you to provision
+	// for a new table that you are creating in this region, including the read
+	// capacity units provisioned for its global secondary indexes (GSIs).
 	TableMaxReadCapacityUnits *int64 `min:"1" type:"long"`
 
-	// The maximum Write-Capacity Units (WCUs) that your account allows you to provision
-	// for a new table you are creating in this region, including the WCUs provisioned
-	// for its global secondary indexes (GSIs).
+	// The maximum write capacity units that your account allows you to provision
+	// for a new table that you are creating in this region, including the write
+	// capacity units provisioned for its global secondary indexes (GSIs).
 	TableMaxWriteCapacityUnits *int64 `min:"1" type:"long"`
 }
 
