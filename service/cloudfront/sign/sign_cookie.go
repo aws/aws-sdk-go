2c347bc2f00dac9407a23bfb23f2d00157197bf8
Quest Henkart <qhenkart@gmail.com>
2016-03-31 12:17:33 -0700

    finished poc implementation of signed cookies

--- service/cloudfront/sign/sign_cookie.go
+++ service/cloudfront/sign/sign_cookie.go
@@ -7,14 +7,72 @@ import (
 
 //the requirements for Cookie Signer is the same for the URL signer
 type CookieSigner struct {
-	*URLSigner
+	keyID   string
+	privKey *rsa.PrivateKey
 }
 
-type Options struct {
+type CookieOptions struct {
 	Path   string
 	Domain string
-	Secure Bool
+	Secure bool
 }
+
+//Example:
+//     // Sign URL to be valid for 30 minutes from now, expires one hour from now, and
+//     // restricted to the 192.0.2.0/24 IP address range.
+
+// func handler(w http.ResponseWriter, r *http.Request) {
+
+// 	//create a policy
+//  //http://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/private-content-setting-signed-cookie-custom-policy.html
+// 	p := &sign.Policy{
+// 		Statements: []sign.Statement{
+// 			{
+//				Resource: RawCloudFrontURL, //read the provided documentation on how to set this correctly, you'll probably want to use wildcards
+// 				Condition: sign.Condition{
+// 					// Optional IP source address range
+// 					IPAddress: &sign.IPAddress{SourceIP: "192.0.2.0/24"},
+// 					// Optional date URL is not valid until
+// 					DateGreaterThan: &sign.AWSEpochTime{time.Now().Add(30 * time.Minute)},
+// 					// Required date the URL will expire after
+// 					DateLessThan: &sign.AWSEpochTime{time.Now().Add(1 * time.Hour)},
+// 				},
+// 			},
+// 		},
+// 	}
+
+// 	//load your private key and convert it to type rsa.PrivateKey
+// 	privKey, err := sign.LoadPEMPrivKeyFile("privatekey.pem")
+// 	if err != nil {
+// 		fmt.Println("error", err)
+// 	}
+
+// 	//key ID that represents the key pair associated with the private key
+// 	keyID := "WOIERULSDLKEWRIU"
+
+// 	//set credentials to the cookiesigner
+// 	signer := sign.NewCookieSigner(keyID, privKey)
+
+// 	//creates 3 signed cookies, provide an optional Cookie Options struct to specify other options
+// 	//http://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/private-content-setting-signed-cookie-custom-policy.html#private-content-custom-policy-statement-signed-cookies-examples
+//  //http://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/CNAMEs.html
+// 	o := &CookieOptions{
+// 		Path:   "/",
+// 		Domain: ".cNameAssociatedWithMyDistribution.com",
+// 		Secure: true, //make sure your app/site can handle https payloads, otherwise set this to false
+// 	}
+
+// 	//avoid adding an Expire or MaxAge. See provided AWS Documentation for more info
+// 	policy, signature, key, err := signer.SignCookies(p)
+// 	if err != nil {
+// 		fmt.Println("error", err)
+// 	}
+
+// 	http.SetCookie(w, policy)
+// 	http.SetCookie(w, signature)
+// 	http.SetCookie(w, key)
+
+// }
 
 // NewCookieSigner constructs and returns a new CookieSigner to be used to for signing
 // Amazon CloudFront URL resources with.
@@ -25,52 +83,59 @@ func NewCookieSigner(keyID string, privKey *rsa.PrivateKey) *CookieSigner {
 	}
 }
 
-//prepares the cookies to be attached to the header. An options struct is provided
+//CookieOptions are optional
+func (c CookieSigner) SignCookies(p *Policy, o *CookieOptions) (cPolicy, cSignature, cKey *http.Cookie, err error) {
+	b64Sig, b64Policy, err := p.Sign(c.privKey)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	cPolicy, cSignature, cKey = c.CreateCookies(b64Policy, b64Sig, o)
+	return cPolicy, cSignature, cKey, nil
+
+}
+
+//prepares the cookies to be attached to the header. An (optional) options struct is provided
 //in case people don't want to manually edit their cookies
 
-func (c CookieSigner) CreateCookies(policy, signature []byte, o *Options) (cPolicy, cSignature, cKey *http.Cookie) {
-	cPolicy := &http.Cookie{
-		Name:   "CloudFront-Policy",
-		Value:  string(policy),
-		Path:   "/",
-		Domain: ".launchpadcentral.com",
-		// Secure:   true,
+func (c CookieSigner) CreateCookies(policy, signature []byte, o *CookieOptions) (cPolicy, cSignature, cKey *http.Cookie) {
+	//creates proper cookies
+	cPolicy = &http.Cookie{
+		Name:     "CloudFront-Policy",
+		Value:    string(policy),
 		HttpOnly: true,
 	}
 
-	cSig := &http.Cookie{
-		Name:   "CloudFront-Signature",
-		Value:  string(signature),
-		Path:   "/",
-		Domain: ".launchpadcentral.com",
-		// Secure:   true,
+	cSignature = &http.Cookie{
+		Name:     "CloudFront-Signature",
+		Value:    string(signature),
 		HttpOnly: true,
 	}
-	cKey := &http.Cookie{
-		Name:   "CloudFront-Key-Pair-Id",
-		Value:  key,
-		Path:   "/",
-		Domain: ".launchpadcentral.com",
-		// Secure:   true,
+	cKey = &http.Cookie{
+		Name:     "CloudFront-Key-Pair-Id",
+		Value:    c.keyID,
 		HttpOnly: true,
 	}
 
-	if Options != nil {
-		if Options.Path != "" {
-			cPolicy.Path = Options.Path
-			cSignature.Path = Options.Path
-			cKey.Path = Options.Path
+	//if options are included, assign them to the cookies
+	if o != nil {
+		if o.Path != "" {
+			cPolicy.Path = o.Path
+			cSignature.Path = o.Path
+			cKey.Path = o.Path
 		}
-		if Options.Domain != "" {
-			cPolicy.Domain = Options.Domain
-			cSignature.Domain = Options.Domain
-			cKey.Domain = Options.Domain
+		if o.Domain != "" {
+			cPolicy.Domain = o.Domain
+			cSignature.Domain = o.Domain
+			cKey.Domain = o.Domain
 		}
-		if Options.Secure != false {
-			cPolicy.Secure = Options.Secure
-			cSignature.Secure = Options.Secure
-			cKey.Secure = Options.Secure
+		if o.Secure != false {
+			cPolicy.Secure = o.Secure
+			cSignature.Secure = o.Secure
+			cKey.Secure = o.Secure
 		}
 	}
-	return cPolicy, cSig, cKey
+
+	//return 3 cookies to be attached to the response
+	return cPolicy, cSignature, cKey
 }
--- service/cloudfront/sign/sign_cookie_test.go
+++ service/cloudfront/sign/sign_cookie_test.go
@@ -1,1 +1,85 @@
 package sign
+
+import (
+	"crypto/rsa"
+	"testing"
+	"time"
+)
+
+const (
+	policyName    = "CloudFront-Policy"
+	signatureName = "CloudFront-Signature"
+	keyName       = "CloudFront-Key-Pair-Id"
+)
+
+func TestSignCookie(t *testing.T) {
+	privKey, err := rsa.GenerateKey(randReader, 1024)
+	if err != nil {
+		t.Fatalf("Unexpected priv key error, %#v", err)
+	}
+
+	signer := NewCookieSigner("keyID", privKey)
+
+	if signer.keyID != "keyID" || signer.privKey != privKey {
+		t.Fatalf("NewCookieSigner does not properly assign values %+v", signer)
+	}
+
+	p := &Policy{
+		Statements: []Statement{
+			{
+				Resource: "*",
+				Condition: Condition{
+					DateLessThan: &AWSEpochTime{time.Now().Add(1 * time.Hour)},
+				},
+			},
+		},
+	}
+	//test cookie signer without any additional options
+	policy, signature, key, err := signer.SignCookies(p, nil)
+	if err != nil {
+		t.Fatalf("Error signing cookies %#v", err)
+	}
+
+	if policy.Name != policyName {
+		t.Fatalf("policy name is incorrect, expected: %s\n got: %s", policyName, policy.Name)
+	}
+	if signature.Name != signatureName {
+		t.Fatalf("signature name is incorrect, expected: %s\n got: %s", signatureName, signature.Name)
+	}
+	if key.Name != keyName {
+		t.Fatalf("key name is incorrect, expected: %s\n got: %s", keyName, key.Name)
+	}
+
+	//test cookie signer with additional options
+	o := &CookieOptions{
+		Path:   "/",
+		Domain: ".myexample.com",
+		Secure: true,
+	}
+
+	policy, signature, key, err = signer.SignCookies(p, o)
+	if err != nil {
+		t.Fatalf("Error signing cookies %#v", err)
+	}
+
+	if policy.Name != policyName {
+		t.Fatalf("policy name is incorrect, expected: %s\n got: %s", policyName, policy.Name)
+	}
+	if signature.Name != signatureName {
+		t.Fatalf("signature name is incorrect, expected: %s\n got: %s", signatureName, signature.Name)
+	}
+	if key.Name != keyName {
+		t.Fatalf("key name is incorrect, expected: %s\n got: %s", keyName, key.Name)
+	}
+
+	if policy.Domain != o.Domain || policy.Path != o.Path || policy.Secure != o.Secure {
+		t.Fatalf("extra options were not applied correctly, expected: %+v\n got: %+v", o, policy)
+	}
+	if signature.Domain != o.Domain || signature.Path != o.Path || signature.Secure != o.Secure {
+		t.Fatalf("extra options were not applied correctly, expected: %+v\n got: %+v", o, signature)
+	}
+	if key.Domain != o.Domain || key.Path != o.Path || key.Secure != o.Secure {
+		t.Fatalf("extra options were not applied correctly, expected: %+v\n got: %+v", o, key)
+	}
+
+}
