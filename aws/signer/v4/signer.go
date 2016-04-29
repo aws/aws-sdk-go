c4192a541f7ca2342c66ca7e3d9549b3fbd5d769
Mike Gaffney <gaffo@justin.tv>
2016-04-29 16:06:40 -0700

    555: base error cases for signer

---
+++ aws/signer/v4/signer.go
@@ -0,0 +1,29 @@
+package v4
+
+import (
+	"net/http"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+)
+
+type Signer struct {
+	Creds *credentials.Credentials
+}
+
+func NewSigner(creds *credentials.Credentials) (*Signer) {
+	return &Signer{
+		Creds: creds,
+	}
+}
+
+func (s Signer) Sign(r *http.Request) error {
+	if s.Creds == nil {
+		return awserr.New("NilCredentials", "Credentials can't be nil", nil)
+	}
+	if r == nil {
+		return awserr.New("NilRequest", "Request can't be nil", nil)
+	}
+
+	return nil
+}
+
---
+++ aws/signer/v4/signer_test.go
@@ -0,0 +1,30 @@
+package v4_test
+
+import (
+	"testing"
+	"net/http"
+	"github.com/stretchr/testify/assert"
+	"github.com/aws/aws-sdk-go/aws/signer/v4"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+)
+
+func TestSignNilCredentials(t *testing.T) {
+	signer := v4.NewSigner(nil)
+	request, err := http.NewRequest("", "", nil)
+	assert.NoError(t, err)
+
+	err = signer.Sign(request)
+
+	assert.Error(t, err)
+}
+
+func TestSignNilRequest(t *testing.T) {
+	creds := credentials.NewStaticCredentials("id", "secret", "token")
+	signer := v4.NewSigner(creds)
+
+	assert.NotNil(t, signer)
+
+	err := signer.Sign(nil)
+
+	assert.Error(t, err)
+}
