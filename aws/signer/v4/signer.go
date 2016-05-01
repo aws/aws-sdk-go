499abe2cd57341f1e684aa2992ada876d6ea1e49
Mike Gaffney <mr.gaffo@gmail.com>
2016-04-30 23:10:08 -0700

    Using a function pointer for setting

--- aws/signer/v4/signer.go
+++ aws/signer/v4/signer.go
@@ -18,12 +18,14 @@ func NewSigner(creds *credentials.Credentials) (*Signer) {
 	}
 }
 
-func set(r *http.Request, presign bool, key, value string) {
-	if presign {
-		r.URL.RawQuery += fmt.Sprintf("&%s=%s", url.QueryEscape(key), url.QueryEscape(value))
-	} else {
-		r.Header.Add(key, value)
-	}
+type setter func(r *http.Request, key, value string)
+
+func setPresign(r *http.Request, key, value string) {
+	r.URL.RawQuery += fmt.Sprintf("&%s=%s", url.QueryEscape(key), url.QueryEscape(value))
+}
+
+func setHeader(r *http.Request, key, value string) {
+	r.Header.Add(key, value)
 }
 
 func (s Signer) Sign(r *http.Request, presign bool) error {
@@ -34,11 +36,16 @@ func (s Signer) Sign(r *http.Request, presign bool) error {
 		return awserr.New("NilRequest", "Request can't be nil", nil)
 	}
 
-	set(r, presign, "X-Amz-Signature", "ea7856749041f727690c580569738282e99c79355fe0d8f125d3b5535d2ece83")
-	set(r, presign, "X-Amz-Credential", "AKID/19700101/us-east-1/dynamodb/aws4_request")
-	set(r, presign, "X-Amz-SignedHeaders", "content-length;content-type;host;x-amz-meta-other-header;x-amz-meta-other-header_with_underscore")
-	set(r, presign, "X-Amz-Date", "19700101T000000Z")
-	set(r, presign, "X-Amz-Target", "prefix.Operation")
+	set := setHeader
+	if presign {
+		set = setPresign
+	}
+
+	set(r, "X-Amz-Signature", "ea7856749041f727690c580569738282e99c79355fe0d8f125d3b5535d2ece83")
+	set(r, "X-Amz-Credential", "AKID/19700101/us-east-1/dynamodb/aws4_request")
+	set(r, "X-Amz-SignedHeaders", "content-length;content-type;host;x-amz-meta-other-header;x-amz-meta-other-header_with_underscore")
+	set(r, "X-Amz-Date", "19700101T000000Z")
+	set(r, "X-Amz-Target", "prefix.Operation")
 
 	return nil
 }
