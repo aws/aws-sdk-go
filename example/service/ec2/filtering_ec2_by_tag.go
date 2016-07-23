f0baa44993756de9c1e1539f9b229c6e24c59396
Stevenson Jean-Pierre <Stevenson.Jean-Pierre@sage.com>
2016-07-22 22:40:36 -0400

    Create filtering_ec2_by_tag.go

---
+++ example/service/ec2/filtering_ec2_by_tag.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"log"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"strings"
+)
+
+var AwsRegions = []string{"us-east-1", "us-west-1", "us-west-2", "eu-west-1", "eu-central-1", "sa-east-1"}
+
+func listFilteredInstances(nameFilter string) []ec2.DescribeInstancesOutput{
+	filteredList := []ec2.DescribeInstancesOutput{}
+	for _, region := range AwsRegions {
+		svc := ec2.New(session.New(&aws.Config{Region: aws.String(region)}))
+		fmt.Println("enumerating VPNs in:", region)
+		params := &ec2.DescribeInstancesInput{
+			Filters: []*ec2.Filter{
+				{
+					Name: aws.String("tag:Name"),
+					Values: []*string{
+						aws.String(strings.Join([]string{"*",nameFilter,"*"},"")),
+					},
+				},
+			},
+		}
+		resp, err := svc.DescribeInstances(params)
+		if err != nil {
+			fmt.Println("there was an error listing instnaces in", region, err.Error())
+			log.Fatal(err.Error())
+		}
+		filteredList = append(filteredList,*resp)
+	}
+	return filteredList
+}
+
+func main() {
+	filteredInstances := listFilteredInstances("vpn")
+	fmt.Printf("%+v\n",filteredInstances)
+}
