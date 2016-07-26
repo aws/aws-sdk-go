a06556fddddbea118af43aac0cf64ca581c88ae1
xibz <impactbchang@gmail.com>
2016-07-25 17:01:02 -0700

    Adding example for filtering by resource tag (#773)
    * Create filtering_ec2_by_tag.go
    
    * Update filtering_ec2_by_tag.go
    
    * Update and rename example/service/ec2/filtering_ec2_by_tag.go to example/service/ec2/filterInstnaces/filter_ec2_by_tag.go
    
    * Create README.md
    
    * Update filter_ec2_by_tag.go
    
    * Update filter_ec2_by_tag.go
    
    * Update filter_ec2_by_tag.go
    
    * Addressed PR comments
    
    * Added comment on how to run
    
    * Adding consistent comment above main function

---
+++ example/service/ec2/filterInstances/filter_ec2_by_tag.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"log"
+	"os"
+	"strings"
+)
+
+// This example will list instances with a filter
+//
+// Usage:
+// go run filter_ec2_by_tag.go <name_filter>
+func main() {
+	nameFilter := os.Args[1]
+	awsRegion := "us-east-1"
+	svc := ec2.New(session.New(&aws.Config{Region: aws.String(awsRegion)}))
+	fmt.Printf("listing instances with tag %v in: %v\n", nameFilter, awsRegion)
+	params := &ec2.DescribeInstancesInput{
+		Filters: []*ec2.Filter{
+			{
+				Name: aws.String("tag:Name"),
+				Values: []*string{
+					aws.String(strings.Join([]string{"*", nameFilter, "*"}, "")),
+				},
+			},
+		},
+	}
+	resp, err := svc.DescribeInstances(params)
+	if err != nil {
+		fmt.Println("there was an error listing instances in", awsRegion, err.Error())
+		log.Fatal(err.Error())
+	}
+	fmt.Printf("%+v\n", *resp)
+}
