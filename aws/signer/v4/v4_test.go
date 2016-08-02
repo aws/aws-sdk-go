aws/aws-sdk-go

commit bae4e5236e96d15b7d97d69b7d91550defa99741

aws/signer/v4: Update V4 signer to sign Sha256 header for S3
The X-Amz-Content-Sha256 header was not being signed for S3 requests.
This header is required to be signed, per the S3 V4 signature spec.

Fix #743

aws/signer/v4/v4.go
@@ -155,6 +155,7 @@ type signingCtx struct {
 	formattedTime      string
 	formattedShortTime string
 
+	bodyDigest       string
 	signedHeaders    string
 	canonicalHeaders string
 	canonicalString  string
@@ -389,6 +390,7 @@ func (ctx *signingCtx) build(disableHeaderHoisting bool) {
 		}
 	}
 
+	ctx.buildBodyDigest()
 	ctx.buildCanonicalHeaders(ignoredHeaders, unsignedHeaders)
 	ctx.buildCanonicalString() // depends on canon headers / signed headers
 	ctx.buildStringToSign()    // depends on canon string
@@ -510,7 +512,7 @@ func (ctx *signingCtx) buildCanonicalString() {
 		ctx.Request.URL.RawQuery,
 		ctx.canonicalHeaders + "\n",
 		ctx.signedHeaders,
-		ctx.bodyDigest(),
+		ctx.bodyDigest,
 	}, "\n")
 }
 
@@ -533,7 +535,7 @@ func (ctx *signingCtx) buildSignature() {
 	ctx.signature = hex.EncodeToString(signature)
 }
 
-func (ctx *signingCtx) bodyDigest() string {
+func (ctx *signingCtx) buildBodyDigest() {
 	hash := ctx.Request.Header.Get("X-Amz-Content-Sha256")
 	if hash == "" {
 		if ctx.isPresign && ctx.ServiceName == "s3" {
@@ -543,9 +545,11 @@ func (ctx *signingCtx) bodyDigest() string {
 		} else {
 			hash = hex.EncodeToString(makeSha256Reader(ctx.Body))
 		}
-		ctx.Request.Header.Add("X-Amz-Content-Sha256", hash)
+		if ctx.ServiceName == "s3" {
+			ctx.Request.Header.Set("X-Amz-Content-Sha256", hash)
+		}
 	}
-	return hash
+	ctx.bodyDigest = hash
 }
 
 // isRequestSigned returns if the request is currently signed or presigned

aws/signer/v4/v4_test.go
@@ -112,9 +112,9 @@ func TestSignRequest(t *testing.T) {
 }
 
 func TestSignBody(t *testing.T) {
-	req, body := buildRequest("dynamodb", "us-east-1", "hello")
+	req, body := buildRequest("s3", "us-east-1", "hello")
 	signer := buildSigner()
-	signer.Sign(req, body, "dynamodb", "us-east-1", time.Now())
+	signer.Sign(req, body, "s3", "us-east-1", time.Now())
 	hash := req.Header.Get("X-Amz-Content-Sha256")
 	assert.Equal(t, "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824", hash)
 }

aws/signer/v4/functional_test.go
@@ -55,12 +55,13 @@ func TestPresignRequest(t *testing.T) {
 	assert.NoError(t, err)
 
 	expectedDate := "19700101T000000Z"
-	expectedHeaders := "content-disposition;host;x-amz-acl"
-	expectedSig := "b2754ba8ffeb74a40b94767017e24c4672107d6d5a894648d5d332ca61f5ffe4"
+	expectedHeaders := "content-disposition;host;x-amz-acl;x-amz-content-sha256"
+	expectedSig := "0d200ba61501d752acd06f39ef4dbe7d83ffd5ea15978dc3476dfc00b8eb574e"
 	expectedCred := "AKID/19700101/mock-region/s3/aws4_request"
 	expectedHeaderMap := http.Header{
-		"x-amz-acl":           []string{"public-read"},
-		"content-disposition": []string{"a+b c$d"},
+		"x-amz-acl":            []string{"public-read"},
+		"content-disposition":  []string{"a+b c$d"},
+		"x-amz-content-sha256": []string{"UNSIGNED-PAYLOAD"},
 	}
 
 	u, _ := url.Parse(urlstr)

commit e11bd1d8a23ff037fd3bd2e2d5fd5c2d68202cd5

fix for issue #783
a regression was introduced in commit
bae4e5236e96d15b7d97d69b7d91550defa99741 (relating to issue #743) that
broke glacier's UploadMultpartPart. I'm not 100% sure what's going on
here, but it appears that glacier has the same signer-v4 exceptions as
s3?

aws/signer/v4/v4.go
@@ -545,7 +545,7 @@ func (ctx *signingCtx) buildBodyDigest() {
 		} else {
 			hash = hex.EncodeToString(makeSha256Reader(ctx.Body))
 		}
-		if ctx.ServiceName == "s3" {
+		if ctx.ServiceName == "s3" || ctx.ServiceName == "glacier" {
 			ctx.Request.Header.Set("X-Amz-Content-Sha256", hash)
 		}
 	}

aws/signer/v4/v4_test.go
@@ -111,10 +111,18 @@ func TestSignRequest(t *testing.T) {
 	assert.Equal(t, expectedDate, q.Get("X-Amz-Date"))
 }
 
-func TestSignBody(t *testing.T) {
+func TestSignBodyS3(t *testing.T) {
 	req, body := buildRequest("s3", "us-east-1", "hello")
 	signer := buildSigner()
 	signer.Sign(req, body, "s3", "us-east-1", time.Now())
+	hash := req.Header.Get("X-Amz-Content-Sha256")
+	assert.Equal(t, "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824", hash)
+}
+
+func TestSignBodyGlacier(t *testing.T) {
+	req, body := buildRequest("glacier", "us-east-1", "hello")
+	signer := buildSigner()
+	signer.Sign(req, body, "glacier", "us-east-1", time.Now())
 	hash := req.Header.Get("X-Amz-Content-Sha256")
 	assert.Equal(t, "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824", hash)
 }