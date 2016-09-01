71321ebfb0b1695c917ef18550c5417cdaa1689d
Jason Del Ponte <delpontej@gmail.com>
2016-09-01 15:25:26 -0700

    service/rds: Update Amazon Relational Database Service model

--- service/rds/api.go
+++ service/rds/api.go
@@ -5053,7 +5053,7 @@ type AddTagsToResourceInput struct {
 
 	// The Amazon RDS resource the tags will be added to. This value is an Amazon
 	// Resource Name (ARN). For information about creating an ARN, see  Constructing
-	// an RDS Amazon Resource Name (ARN) (http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_Tagging.html#USER_Tagging.ARN).
+	// an RDS Amazon Resource Name (ARN) (http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_Tagging.ARN.html#USER_Tagging.ARN.Constructing).
 	ResourceName *string `type:"string" required:"true"`
 
 	// The tags to be assigned to the Amazon RDS resource.
@@ -5123,7 +5123,7 @@ type ApplyPendingMaintenanceActionInput struct {
 
 	// The RDS Amazon Resource Name (ARN) of the resource that the pending maintenance
 	// action applies to. For information about creating an ARN, see  Constructing
-	// an RDS Amazon Resource Name (ARN) (http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_Tagging.html#USER_Tagging.ARN).
+	// an RDS Amazon Resource Name (ARN) (http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_Tagging.ARN.html#USER_Tagging.ARN.Constructing).
 	ResourceIdentifier *string `type:"string" required:"true"`
 }
 
@@ -5277,6 +5277,7 @@ func (s AvailabilityZone) GoString() string {
 type Certificate struct {
 	_ struct{} `type:"structure"`
 
+	// The Amazon Resource Name (ARN) for the certificate.
 	CertificateArn *string `type:"string"`
 
 	// The unique key that identifies a certificate.
@@ -5331,7 +5332,7 @@ type CopyDBClusterParameterGroupInput struct {
 
 	// The identifier or Amazon Resource Name (ARN) for the source DB cluster parameter
 	// group. For information about creating an ARN, see  Constructing an RDS Amazon
-	// Resource Name (ARN) (http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_Tagging.html#USER_Tagging.ARN).
+	// Resource Name (ARN) (http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_Tagging.ARN.html#USER_Tagging.ARN.Constructing).
 	//
 	// Constraints:
 	//
@@ -5508,18 +5509,14 @@ type CopyDBParameterGroupInput struct {
 
 	// The identifier or ARN for the source DB parameter group. For information
 	// about creating an ARN, see  Constructing an RDS Amazon Resource Name (ARN)
-	// (http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_Tagging.html#USER_Tagging.ARN).
+	// (http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_Tagging.ARN.html#USER_Tagging.ARN.Constructing).
 	//
 	// Constraints:
 	//
 	//   Must specify a valid DB parameter group.
 	//
-	//   If the source DB parameter group is in the same region as the copy, specify
-	// a valid DB parameter group identifier, for example my-db-param-group, or
-	// a valid ARN.
-	//
-	//   If the source DB parameter group is in a different region than the copy,
-	// specify a valid DB parameter group ARN, for example arn:aws:rds:us-west-2:123456789012:pg:special-parameters.
+	//    Must specify a valid DB parameter group identifier, for example my-db-param-group,
+	// or a valid ARN.
 	SourceDBParameterGroupIdentifier *string `type:"string" required:"true"`
 
 	// A list of tags.
@@ -5712,7 +5709,7 @@ type CopyOptionGroupInput struct {
 	_ struct{} `type:"structure"`
 
 	// The identifier or ARN for the source option group. For information about
-	// creating an ARN, see  Constructing an RDS Amazon Resource Name (ARN) (http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_Tagging.html#USER_Tagging.ARN).
+	// creating an ARN, see  Constructing an RDS Amazon Resource Name (ARN) (http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_Tagging.ARN.html#USER_Tagging.ARN.Constructing).
 	//
 	// Constraints:
 	//
@@ -6463,10 +6460,6 @@ type CreateDBInstanceInput struct {
 	//
 	//    Version 5.5 (available in all AWS regions):  5.5.46
 	//
-	//    Version 5.1 (only available in AWS regions ap-northeast-1, ap-southeast-1,
-	// ap-southeast-2, eu-west-1, sa-east-1, us-east-1, us-gov-west-1, us-west-1,
-	// us-west-2):  5.1.73a | 5.1.73b
-	//
 	//    Oracle Database Enterprise Edition (oracle-ee)
 	//
 	//    Version 12.1 (available in all AWS regions except ap-south-1, ap-northeast-2):
@@ -6480,17 +6473,6 @@ type CreateDBInstanceInput struct {
 	//
 	//    Version 12.1 (available in all AWS regions except us-gov-west-1):  12.1.0.2.v2
 	// | 12.1.0.2.v3 | 12.1.0.2.v4
-	//
-	//    Version 11.2 (only available in AWS regions ap-northeast-1, ap-southeast-1,
-	// ap-southeast-2, eu-west-1, sa-east-1, us-east-1, us-gov-west-1, us-west-1,
-	// us-west-2):  11.2.0.2.v3 | 11.2.0.2.v4 | 11.2.0.2.v5 | 11.2.0.2.v6 | 11.2.0.2.v7
-	//
-	//    Version 11.2 (available in all AWS regions except ap-south-1, ap-northeast-2):
-	//  11.2.0.3.v1 | 11.2.0.3.v2 | 11.2.0.3.v3
-	//
-	//    Version 11.2 (only available in AWS regions ap-northeast-1, ap-southeast-1,
-	// ap-southeast-2, eu-central-1, eu-west-1, sa-east-1, us-east-1, us-west-1,
-	// us-west-2):  11.2.0.3.v4
 	//
 	//    Version 11.2 (available in all AWS regions):  11.2.0.4.v1 | 11.2.0.4.v3
 	// | 11.2.0.4.v4
@@ -6507,17 +6489,6 @@ type CreateDBInstanceInput struct {
 	// ap-southeast-2, eu-central-1, eu-west-1, sa-east-1, us-east-1, us-west-1,
 	// us-west-2):  12.1.0.1.v3 | 12.1.0.1.v4 | 12.1.0.1.v5
 	//
-	//    Version 11.2 (only available in AWS regions ap-northeast-1, ap-southeast-1,
-	// ap-southeast-2, eu-west-1, sa-east-1, us-east-1, us-gov-west-1, us-west-1,
-	// us-west-2):  11.2.0.2.v3 | 11.2.0.2.v4 | 11.2.0.2.v5 | 11.2.0.2.v6 | 11.2.0.2.v7
-	//
-	//    Version 11.2 (available in all AWS regions except ap-south-1, ap-northeast-2):
-	//  11.2.0.3.v1 | 11.2.0.3.v2 | 11.2.0.3.v3
-	//
-	//    Version 11.2 (only available in AWS regions ap-northeast-1, ap-southeast-1,
-	// ap-southeast-2, eu-central-1, eu-west-1, sa-east-1, us-east-1, us-west-1,
-	// us-west-2):  11.2.0.3.v4
-	//
 	//    Version 11.2 (available in all AWS regions):  11.2.0.4.v1 | 11.2.0.4.v3
 	// | 11.2.0.4.v4
 	//
@@ -6532,17 +6503,6 @@ type CreateDBInstanceInput struct {
 	//    Version 12.1 (only available in AWS regions ap-northeast-1, ap-southeast-1,
 	// ap-southeast-2, eu-central-1, eu-west-1, sa-east-1, us-east-1, us-west-1,
 	// us-west-2):  12.1.0.1.v3 | 12.1.0.1.v4 | 12.1.0.1.v5
-	//
-	//    Version 11.2 (only available in AWS regions ap-northeast-1, ap-southeast-1,
-	// ap-southeast-2, eu-west-1, sa-east-1, us-east-1, us-gov-west-1, us-west-1,
-	// us-west-2):  11.2.0.2.v3 | 11.2.0.2.v4 | 11.2.0.2.v5 | 11.2.0.2.v6 | 11.2.0.2.v7
-	//
-	//    Version 11.2 (available in all AWS regions except ap-south-1, ap-northeast-2):
-	//  11.2.0.3.v1 | 11.2.0.3.v2 | 11.2.0.3.v3
-	//
-	//    Version 11.2 (only available in AWS regions ap-northeast-1, ap-southeast-1,
-	// ap-southeast-2, eu-central-1, eu-west-1, sa-east-1, us-east-1, us-west-1,
-	// us-west-2):  11.2.0.3.v4
 	//
 	//    Version 11.2 (available in all AWS regions):  11.2.0.4.v1 | 11.2.0.4.v3
 	// | 11.2.0.4.v4
@@ -7055,7 +7015,7 @@ type CreateDBInstanceReadReplicaInput struct {
 	//
 	//   If the source DB instance is in a different region than the Read Replica,
 	// specify a valid DB instance ARN. For more information, go to  Constructing
-	// a Amazon RDS Amazon Resource Name (ARN) (http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_Tagging.html#USER_Tagging.ARN).
+	// a Amazon RDS Amazon Resource Name (ARN) (http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_Tagging.ARN.html#USER_Tagging.ARN.Constructing).
 	SourceDBInstanceIdentifier *string `type:"string" required:"true"`
 
 	// Specifies the storage type to be associated with the Read Replica.
@@ -7656,6 +7616,7 @@ type DBCluster struct {
 	// associated with.
 	CharacterSetName *string `type:"string"`
 
+	// The Amazon Resource Name (ARN) for the DB cluster.
 	DBClusterArn *string `type:"string"`
 
 	// Contains a user-supplied DB cluster identifier. This identifier is the unique
@@ -7815,6 +7776,7 @@ func (s DBClusterOptionGroupStatus) GoString() string {
 type DBClusterParameterGroup struct {
 	_ struct{} `type:"structure"`
 
+	// The Amazon Resource Name (ARN) for the DB cluster parameter group.
 	DBClusterParameterGroupArn *string `type:"string"`
 
 	// Provides the name of the DB cluster parameter group.
@@ -7892,6 +7854,7 @@ type DBClusterSnapshot struct {
 	// snapshot was created from.
 	DBClusterIdentifier *string `type:"string"`
 
+	// The Amazon Resource Name (ARN) for the DB cluster snapshot.
 	DBClusterSnapshotArn *string `type:"string"`
 
 	// Specifies the identifier for the DB cluster snapshot.
@@ -8091,6 +8054,7 @@ type DBInstance struct {
 	// DB cluster that the DB instance is a member of.
 	DBClusterIdentifier *string `type:"string"`
 
+	// The Amazon Resource Name (ARN) for the DB instance.
 	DBInstanceArn *string `type:"string"`
 
 	// Contains the name of the compute and memory capacity class of the DB instance.
@@ -8307,6 +8271,7 @@ func (s DBInstanceStatusInfo) GoString() string {
 type DBParameterGroup struct {
 	_ struct{} `type:"structure"`
 
+	// The Amazon Resource Name (ARN) for the DB parameter group.
 	DBParameterGroupArn *string `type:"string"`
 
 	// Provides the name of the DB parameter group family that this DB parameter
@@ -8399,6 +8364,7 @@ func (s DBParameterGroupStatus) GoString() string {
 type DBSecurityGroup struct {
 	_ struct{} `type:"structure"`
 
+	// The Amazon Resource Name (ARN) for the DB security group.
 	DBSecurityGroupArn *string `type:"string"`
 
 	// Provides the description of the DB security group.
@@ -8481,6 +8447,7 @@ type DBSnapshot struct {
 	// was created from.
 	DBInstanceIdentifier *string `type:"string"`
 
+	// The Amazon Resource Name (ARN) for the DB snapshot.
 	DBSnapshotArn *string `type:"string"`
 
 	// Specifies the identifier for the DB snapshot.
@@ -8634,6 +8601,7 @@ func (s DBSnapshotAttributesResult) GoString() string {
 type DBSubnetGroup struct {
 	_ struct{} `type:"structure"`
 
+	// The Amazon Resource Name (ARN) for the DB subnet group.
 	DBSubnetGroupArn *string `type:"string"`
 
 	// Provides the description of the DB subnet group.
@@ -11910,6 +11878,7 @@ type Event struct {
 	// Provides the text of this event.
 	Message *string `type:"string"`
 
+	// The Amazon Resource Name (ARN) for the event.
 	SourceArn *string `type:"string"`
 
 	// Provides the identifier for the source of the event.
@@ -11969,6 +11938,7 @@ type EventSubscription struct {
 	// A list of event categories for the RDS event notification subscription.
 	EventCategoriesList []*string `locationNameList:"EventCategory" type:"list"`
 
+	// The Amazon Resource Name (ARN) for the event subscription.
 	EventSubscriptionArn *string `type:"string"`
 
 	// The topic ARN of the RDS event notification subscription.
@@ -12137,7 +12107,7 @@ type ListTagsForResourceInput struct {
 
 	// The Amazon RDS resource with tags to be listed. This value is an Amazon Resource
 	// Name (ARN). For information about creating an ARN, see  Constructing an RDS
-	// Amazon Resource Name (ARN) (http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_Tagging.html#USER_Tagging.ARN).
+	// Amazon Resource Name (ARN) (http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_Tagging.ARN.html#USER_Tagging.ARN.Constructing).
 	ResourceName *string `type:"string" required:"true"`
 }
 
@@ -13471,6 +13441,7 @@ type OptionGroup struct {
 	// Indicates the major engine version associated with this option group.
 	MajorEngineVersion *string `type:"string"`
 
+	// The Amazon Resource Name (ARN) for the option group.
 	OptionGroupArn *string `type:"string"`
 
 	// Provides a description of the option group.
@@ -13545,12 +13516,15 @@ type OptionGroupOption struct {
 	// The name of the option.
 	Name *string `type:"string"`
 
-	// Specifies the option settings that are available (and the default value)
-	// for each option in an option group.
+	// The option settings that are available (and the default value) for each option
+	// in an option group.
 	OptionGroupOptionSettings []*OptionGroupOptionSetting `locationNameList:"OptionGroupOptionSetting" type:"list"`
 
 	// The versions that are available for the option.
 	OptionGroupOptionVersions []*OptionVersion `locationNameList:"OptionVersion" type:"list"`
+
+	// The options that conflict with this option.
+	OptionsConflictsWith []*string `locationNameList:"OptionConflictName" type:"list"`
 
 	// The options that are prerequisites for this option.
 	OptionsDependedOn []*string `locationNameList:"OptionName" type:"list"`
@@ -14280,7 +14254,7 @@ type RemoveTagsFromResourceInput struct {
 
 	// The Amazon RDS resource the tags will be removed from. This value is an Amazon
 	// Resource Name (ARN). For information about creating an ARN, see  Constructing
-	// an RDS Amazon Resource Name (ARN) (http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_Tagging.html#USER_Tagging.ARN).
+	// an RDS Amazon Resource Name (ARN) (http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_Tagging.ARN.html#USER_Tagging.ARN.Constructing).
 	ResourceName *string `type:"string" required:"true"`
 
 	// The tag key (name) of the tag to be removed.
@@ -14359,6 +14333,7 @@ type ReservedDBInstance struct {
 	// The recurring price charged to run this reserved DB instance.
 	RecurringCharges []*RecurringCharge `locationNameList:"RecurringCharge" type:"list"`
 
+	// The Amazon Resource Name (ARN) for the reserved DB instance.
 	ReservedDBInstanceArn *string `type:"string"`
 
 	// The unique identifier for the reservation.
