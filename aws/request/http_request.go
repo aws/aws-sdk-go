71e5a5edc1a6948c9028ec898081c2a7fbd4143d
ryanfowler <ryan.fowler19@gmail.com>
2016-09-16 07:09:15 -0400

    Take shallow copy of HTTP request when copying
    Taking a shallow copy of the HTTP request copies over all fields,
    making this library compatible with 'context' in go 1.7+.

--- aws/request/http_request.go
+++ aws/request/http_request.go
@@ -1,28 +1,17 @@
-// +build go1.5
-
 package request
 
 import (
 	"io"
 	"net/http"
-	"net/url"
 )
 
 func copyHTTPRequest(r *http.Request, body io.ReadCloser) *http.Request {
-	req := &http.Request{
-		URL:           &url.URL{},
-		Header:        http.Header{},
-		Close:         r.Close,
-		Body:          body,
-		Host:          r.Host,
-		Method:        r.Method,
-		Proto:         r.Proto,
-		ContentLength: r.ContentLength,
-		// Cancel will be deprecated in 1.7 and will be replaced with Context
-		Cancel: r.Cancel,
-	}
-
+	req := new(http.Request)
+	*req = *r
 	*req.URL = *r.URL
+	req.Body = body
+
+	req.Header = http.Header{}
 	for k, v := range r.Header {
 		for _, vv := range v {
 			req.Header.Add(k, vv)
