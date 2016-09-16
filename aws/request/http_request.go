3a3679d5d1ffdf8c7693eb98844a13bafc4be3b3
ryanfowler <ryan.fowler19@gmail.com>
2016-09-16 07:31:21 -0400

    Shallow copy URL field when copying HTTP request

--- aws/request/http_request.go
+++ aws/request/http_request.go
@@ -3,11 +3,13 @@ package request
 import (
 	"io"
 	"net/http"
+	"net/url"
 )
 
 func copyHTTPRequest(r *http.Request, body io.ReadCloser) *http.Request {
 	req := new(http.Request)
 	*req = *r
+	req.URL = &url.URL{}
 	*req.URL = *r.URL
 	req.Body = body
 
