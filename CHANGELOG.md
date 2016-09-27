e7138b3747f8bdeadfb537d9f8d0a84c035aa463
Baron Von Ben Powell <powellbe@amazon.com>
2016-09-27 23:17:17 +0000

    1.4.13

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+Release v1.4.13 (2016-09-27)
+---
+* `service/codepipeline`:  documentation updates.
+* `service/cloudformation`:  api and documentation updates.
+* `service/kms`:  documentation updates.
+* `service/elasticfilesystem`:  documentation updates.
+* `service/snowball`:  documentation updates.
--- service/snowball/service.go
+++ service/snowball/service.go
@@ -11,7 +11,14 @@ import (
 	"github.com/aws/aws-sdk-go/private/protocol/jsonrpc"
 )
 
-// This is a test of the welcome page front matter.
+// AWS Import/Export Snowball is a petabyte-scale data transport solution that
+// uses secure appliances to transfer large amounts of data between your on-premises
+// data centers and Amazon Simple Storage Service (Amazon S3). The Snowball
+// commands described here provide access to the same functionality that is
+// available in the AWS Snowball Management Console, which enables you to create
+// and manage jobs for Snowball. To transfer data locally with a Snowball appliance,
+// you'll need to use the Snowball client or the Amazon S3 API adapter for Snowball.
+// For more information, see the User Guide (http://docs.aws.amazon.com/AWSImportExport/latest/ug/api-reference.html).
 //The service client's operations are safe to be used concurrently.
 // It is not safe to mutate any of the client's properties though.
 type Snowball struct {
