af23041eb0d76bf0e8b2285beccb18f676b310e1
Baron Von Ben Powell <powellbe@amazon.com>
2016-11-17 21:12:33 +0000

    Updating base branch :

--- service/rds/customizations.go
+++ service/rds/customizations.go
@@ -4,7 +4,6 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awsutil"
 	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/private/endpoints"
 )
