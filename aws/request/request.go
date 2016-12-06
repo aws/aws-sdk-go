179a21e34fdebc7f72c03125bb0ef6db8b8a8564
xibz <impactbchang@gmail.com>
2016-12-06 08:58:39 -0800

    Fixing bug #947

--- aws/request/request.go
+++ aws/request/request.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"net/url"
 	"reflect"
@@ -297,7 +298,7 @@ func (r *Request) Send() error {
 
 		r.Handlers.Send.Run(r)
 		if r.Error != nil {
-			if strings.Contains(r.Error.Error(), "net/http: request canceled") {
+			if !shouldRetryCancel(r) {
 				return r.Error
 			}
 
@@ -363,4 +364,19 @@ func AddToUserAgent(r *Request, s string) {
 		s = curUA + " " + s
 	}
 	r.HTTPRequest.Header.Set("User-Agent", s)
+}
+
+func shouldRetryCancel(r *Request) bool {
+	awsErr, ok := r.Error.(awserr.Error)
+	timeoutErr := false
+	if ok {
+		netErr, netOK := awsErr.OrigErr().(net.Error)
+		timeoutErr = netOK && netErr.Timeout()
+	}
+
+	// There can be two types of canceled errors here.
+	// The first being a net.Error and the other being an error.
+	// If the request was timed out, we want to continue the retry
+	// process. Otherwise, return the canceled error.
+	return timeoutErr || !strings.Contains(r.Error.Error(), "net/http: request canceled")
 }
