6b1f0ee36eefb6b877191a3efb4ed9991f4121e4
Jason Del Ponte <delpontej@gmail.com>
2016-12-09 13:25:20 -0800

    add docs

--- aws/request/request_resetbody.go
+++ aws/request/request_resetbody.go
@@ -7,6 +7,19 @@ import (
 	"net/http"
 )
 
+// Go 1.8 tightened and clarified the rules code needs to use when building
+// requests with the http package. Go 1.8 removed the automatic detection
+// of if the Request.Body was empty, or actually had bytes in it. The SDK
+// always sets the Request.Body even if it is empty and should not actually
+// be sent. This is incorrect.
+//
+// Go 1.8 did add a http.NoBody value that the SDK can use to tell the http
+// client that the request really should be sent without a body. The
+// Request.Body cannot be set to nil, which is preferable, because the
+// field is exported and could introduce nil pointer dereferences for users
+// of the SDK if they used that field.
+//
+// Related golang/go#18257
 func resetBody(r *Request) {
 	curOffset, _ := r.Body.Seek(0, io.SeekCurrent)
 	endOffset, _ := r.Body.Seek(0, io.SeekEnd)
--- aws/request/request_resetbody_1.7.go
+++ aws/request/request_resetbody_1.7.go
@@ -2,6 +2,9 @@
 
 package request
 
+// Pre Go 1.8 funcitonality to always set Request.Body to a value regardless
+// if it will actually be used. Go 1.8 removed undocumented feature the SDK
+// expected where the request body would only be sent if the length was > 0.
 func resetBody(r *Request) {
 	r.safeBody = newOffsetReader(r.Body, r.BodyStart)
 	r.HTTPRequest.Body = r.safeBody
