e2d64a023ae871d8707a43893a69d2ccec18b68d
Jason Del Ponte <delpontej@gmail.com>
2016-12-12 14:36:28 -0800

    simplified implementation

--- aws/request/request_1_8_test.go
+++ aws/request/request_1_8_test.go
@@ -1,3 +1,5 @@
+// +build go1.8
+
 package request
 
 import (
@@ -6,17 +8,17 @@ import (
 	"testing"
 )
 
-func TestResetBody_WithBodyContents(t *testing.T) {
+func TestResetBody_WithEmptyBody(t *testing.T) {
 	r := Request{
 		HTTPRequest: &http.Request{},
 	}
 
-	reader := strings.NewReader("abc")
+	reader := strings.NewReader("")
 	r.Body = reader
 
 	r.ResetBody()
 
-	if v, ok := r.HTTPRequest.Body.(*offsetReader); !ok || v == nil {
+	if a, e := r.HTTPRequest.Body, http.NoBody; a != e {
 		t.Errorf("expected request body to be set to reader, got %#v",
 			r.HTTPRequest.Body)
 	}
