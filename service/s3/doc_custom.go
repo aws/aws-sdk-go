b0f9a907a786d509b8c83d0157ca51ca7fc70b96
Jason Del Ponte <delpontej@gmail.com>
2017-04-28 13:24:29 -0700

    s3 custom docs

--- service/s3/doc_custom.go
+++ service/s3/doc_custom.go
@@ -1,6 +1,5 @@
 // S3 Upload and Download Managers
 //
-//
 // See the s3manager package documentation for more information.
 // https://docs.aws.amazon.com/sdk-for-go/api/service/s3/s3manager/
 //
@@ -8,4 +7,9 @@
 //
 // See the s3crypto package documentation for more information.
 // https://docs.aws.amazon.com/sdk-for-go/api/service/s3/s3crypto/
+//
+// Get Bucket Region
+//
+// See the s3manager package's GetBucketRegion function documentation for more information
+// https://docs.aws.amazon.com/sdk-for-go/api/service/s3/s3manager/#GetBucketRegion
 package s3
