0823c21b432661aa589136b54becc3bcf8b727b0
Radek Simko <radek.simko@gmail.com>
2017-05-19 08:51:43 +0200

    Treat SnapshotCreationPerVolumeRateExceeded as retryable code

--- aws/request/retryer.go
+++ aws/request/retryer.go
@@ -40,6 +40,7 @@ var throttleCodes = map[string]struct{}{
 	"RequestThrottled":                       {},
 	"TooManyRequestsException":               {}, // Lambda functions
 	"PriorRequestNotComplete":                {}, // Route53
+	"SnapshotCreationPerVolumeRateExceeded":  {}, // EBS volume snapshots
 }
 
 // credsExpiredCodes is a collection of error codes which signify the credentials
