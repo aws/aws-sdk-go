bdf44b1485597213dca1cf29fc9f1acd9275b535
Jason Del Ponte <delpontej@gmail.com>
2017-06-05 16:45:56 -0700

    fixup examples

--- example/aws/credentials/plugincreds/main.go
+++ example/aws/credentials/plugincreds/main.go
@@ -4,11 +4,13 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"plugin"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials/plugincreds"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 // Example application which loads a Go Plugin file, and uses the credential
@@ -16,22 +18,26 @@ import (
 // request.
 //
 // Build:
-//   go build -tags example -o usePlugin usePlugin.go
+//   go build -tags example -o usePlugin main.go
 //
 // Usage:
-//   ./usePlugin <compiled plugin file>
+//   ./usePlugin <compiled plugin>
 func main() {
+	if len(os.Args) < 2 {
+		exitErrorPrintf("Usage: usePlugin <compiled plugin>")
+	}
+
 	// Open plugin, and load it into the process.
-	p, err := plugin.Open("somefile.so")
+	p, err := plugin.Open(os.Args[1])
 	if err != nil {
-		return nil, err
+		exitErrorPrintf("failed to open plugin, %s, %v", os.Args[1], err)
 	}
 
 	// Create a new Credentials value which will source the provider's Retrieve
 	// and IsExpired functions from the plugin.
 	creds, err := plugincreds.NewCredentials(p)
 	if err != nil {
-		return nil, err
+		exitErrorPrintf("failed to load plugin provider, %v", err)
 	}
 
 	// Example to configure a Session with the newly created credentials that
@@ -41,11 +47,15 @@ func main() {
 	}))
 
 	svc := s3.New(sess)
-
-	result, err := svc.HeadObject(&s3.HeadObject{
+	result, err := svc.HeadObject(&s3.HeadObjectInput{
 		Bucket: aws.String("myBucket"),
 		Key:    aws.String("myKey"),
 	})
 
 	fmt.Println(result, err)
+}
+
+func exitErrorPrintf(format string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, args...)
+	os.Exit(1)
 }
