54ed40baa93038518bb82e7bb60f9b11ee3e9a12
Hajime Hayano <hayanoh@amazon.com>
2017-07-25 08:58:28 -0700

    write Base32Encoding test cases

--- service/dynamodb/expression/operand_test.go
+++ service/dynamodb/expression/operand_test.go
@@ -1,10 +1,12 @@
 package expression
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 func TestBuildOperand(t *testing.T) {
@@ -19,6 +21,71 @@ func TestBuildOperand(t *testing.T) {
 					"#" + encode("foo"): aws.String("foo"),
 				},
 				Expression: "#" + encode("foo"),
+			},
+		},
+		{
+			input: NewValue(5),
+			expected: Expression{
+				Values: map[string]*dynamodb.AttributeValue{
+					":" + encode(fmt.Sprint(dynamodb.AttributeValue{
+						N: aws.String("5"),
+					})): &dynamodb.AttributeValue{
+						N: aws.String("5"),
+					},
+				},
+				Expression: ":" + encode(fmt.Sprint(dynamodb.AttributeValue{
+					N: aws.String("5"),
+				})),
+			},
+		},
+		{
+			input: NewPath("foo.bar[2].baz"),
+			expected: Expression{
+				Names: map[string]*string{
+					"#" + encode("foo"): aws.String("foo"),
+					"#" + encode("bar"): aws.String("bar"),
+					"#" + encode("baz"): aws.String("baz"),
+				},
+				Expression: "#" + encode("foo") + ".#" + encode("bar") + "[2]" + ".#" + encode("baz"),
+			},
+		},
+		{
+			input: NewValue(map[string]int{
+				"even": 2,
+				"odd":  1,
+			}),
+			expected: Expression{
+				Values: map[string]*dynamodb.AttributeValue{
+					":" + encode(fmt.Sprint(dynamodb.AttributeValue{
+						M: map[string]*dynamodb.AttributeValue{
+							"even": &dynamodb.AttributeValue{
+								N: aws.String("2"),
+							},
+							"odd": &dynamodb.AttributeValue{
+								N: aws.String("1"),
+							},
+						},
+					})): &dynamodb.AttributeValue{
+						M: map[string]*dynamodb.AttributeValue{
+							"even": &dynamodb.AttributeValue{
+								N: aws.String("2"),
+							},
+							"odd": &dynamodb.AttributeValue{
+								N: aws.String("1"),
+							},
+						},
+					},
+				},
+				Expression: ":" + encode(fmt.Sprint(dynamodb.AttributeValue{
+					M: map[string]*dynamodb.AttributeValue{
+						"even": &dynamodb.AttributeValue{
+							N: aws.String("2"),
+						},
+						"odd": &dynamodb.AttributeValue{
+							N: aws.String("1"),
+						},
+					},
+				})),
 			},
 		},
 	}
