dfb8dc59c289ec307368e8399f5433ac8e82aa02
piusranjan <pius.satpathy@gmail.com>
2017-08-20 17:35:40 +0300

    Program to find the all the ec2 instace and its state by region

--- example/service/ec2/filterInstances/README.md
+++ example/service/ec2/filterInstances/README.md
@@ -29,3 +29,32 @@ listing instances with tag vpn in: us-east-1
         }],
       ...
 ```
+
+# Example  Fetch By region
+
+This is an example using the AWS SDK for Go to list ec2 instances instance state By different region . By default it fetch all running and stopped instance 
+
+
+# Usage
+
+The example uses the bucket name provided, and lists all object keys in a bucket.
+
+```sh
+go run  filter_ec2_by_region running
+```
+
+Output:
+```
+
+Fetching instace details  for region:  ap-south-1
+printing instance details.....
+instance id i-00cf3fcsssdd373766
+current State stopped
+done for region ap-south-1 ****
+
+
+
+Fetching instace details  for region:  eu-west-2
+There is no instance for the for region:  eu-west-2
+done for region eu-west-2 ****
+
---
+++ example/service/ec2/filterInstances/filter_ec2_by_region.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+    "fmt"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/ec2"
+    "github.com/aws/aws-sdk-go/aws"
+    "os"
+)
+
+func FetchRegion()[]string{
+
+	regions := []string{}  
+	sess1 := session.Must(session.NewSession(&aws.Config{
+			Region: aws.String("us-west-1"),
+	}))
+
+	svc := ec2.New(sess1)
+        regions_1, err := svc.DescribeRegions(&ec2.DescribeRegionsInput{})
+ 	if err != nil {
+        	fmt.Println("Error", err)
+    	}  
+        for _, region := range regions_1.Regions {
+                 regions=append(regions,*region.RegionName) 
+         }
+
+ 	return regions
+}
+
+func main() {
+
+	States:= []*string{}
+	if len(os.Args)>1  {
+		for i := 1; i < len(os.Args); i++ {
+			States=append(States,&os.Args[i])
+			
+		}	
+	} else {
+		States= []*string{
+				 aws.String("running"),
+				 aws.String("pending"),
+				 aws.String("stopped"),
+				 }
+	       }					
+    	  
+
+        regions:=FetchRegion()
+	for _, region := range regions {
+		sess := session.Must(session.NewSession(&aws.Config{
+			Region: aws.String(region),
+		}))
+   	 	
+   		ec2Svc := ec2.New(sess)
+		params := &ec2.DescribeInstancesInput{
+        	Filters: []*ec2.Filter{
+            		&ec2.Filter{
+                		Name: aws.String("instance-state-name"),
+                		Values: States,
+           		 },
+        		},
+    		}
+    
+    	result, err := ec2Svc.DescribeInstances(params)
+    	if err != nil {
+        	fmt.Println("Error", err)
+    	} else {
+		fmt.Println("\n\n\nFetching instace details  for region: ", region )
+              	if len(result.Reservations)==0{
+			fmt.Println("There is no instance for the for region: ", region )
+                }
+	      	for _, reservation := range result.Reservations {
+	   	  
+		    fmt.Println("printing instance details.....")
+		    for _, instance := range reservation.Instances {
+			fmt.Println("instance id "+*instance.InstanceId)
+			fmt.Println("current State "+*instance.State.Name)
+		    }		
+	        }
+	       fmt.Printf("done for region %s **** \n", region )     
+          } 
+     }
+ }
+
