dbba0202bd15dffc44a788f23ca0bc4a938522c1
xibz <impactbchang@gmail.com>
2018-06-11 14:26:26 -0700

    Adding builer to allow for generating auth tokens more easily

--- service/rds/rdsutils/example_test.go
+++ service/rds/rdsutils/example_test.go
@@ -1,23 +1,30 @@
+// +build example,exclude
+
 package rdsutils_test
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"database/sql"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 	"os"
 
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-sql-driver/mysql"
 
 	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/rds/rdsutils"
 )
 
-// Example contains usage of assuming a role and using
+// ExampleConnectionStringBuilder contains usage of assuming a role and using
 // that to build the auth token.
 // Usage:
 //	./main -user "iamuser" -dbname "foo" -region "us-west-2" -rolearn "arn" -endpoint "dbendpoint" -port 3306
-func ExampleRDSUtils_ConnectViaAssumeRole() {
+func ExampleConnectionStringBuilder() {
 	userPtr := flag.String("user", "", "user of the credentials")
 	regionPtr := flag.String("region", "us-east-1", "region to be used when grabbing sts creds")
 	roleArnPtr := flag.String("rolearn", "", "role arn to be used when grabbing sts creds")
@@ -42,23 +49,25 @@ func ExampleRDSUtils_ConnectViaAssumeRole() {
 		os.Exit(1)
 	}
 
+	err := registerRDSMysqlCerts(http.DefaultClient)
+	if err != nil {
+		panic(err)
+	}
+
 	sess := session.Must(session.NewSession())
 	creds := stscreds.NewCredentials(sess, *roleArnPtr)
 
+	v := url.Values{}
+	// required fields for DB connection
+	v.Add("tls", "rds")
+	v.Add("allowCleartextPasswords", "true")
 	endpoint := fmt.Sprintf("%s:%d", *endpointPtr, *portPtr)
-	token, err := rdsutils.BuildAuthToken(endpoint, *regionPtr, *userPtr, creds)
-	if err != nil {
-		panic(fmt.Errorf("failed to build authentication token: %v", err))
-	}
 
-	// builds the connection endpoint for the SQL driver to use.
-	dnsStr := fmt.Sprintf("%s:%s@tcp(%s)/%s?tls=true",
-		*userPtr, token, *endpointPtr, *dbNamePtr,
-	)
+	b := rdsutils.NewConnectionStringBuilder(endpoint, *regionPtr, *userPtr, *dbNamePtr, creds)
+	connectStr, err := b.WithTCPFormat().WithParams(v).Build()
 
 	const dbType = "mysql"
-
-	db, err := sql.Open(dbType, dnsStr)
+	db, err := sql.Open(dbType, connectStr)
 	// if an error is encountered here, then most likely security groups are incorrect
 	// in the database.
 	if err != nil {
@@ -88,4 +97,23 @@ func requiredFlags(flags ...interface{}) error {
 		}
 	}
 	return nil
+}
+
+func registerRDSMysqlCerts(c *http.Client) error {
+	resp, err := c.Get("https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem")
+	if err != nil {
+		return err
+	}
+
+	pem, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	rootCertPool := x509.NewCertPool()
+	if ok := rootCertPool.AppendCertsFromPEM(pem); !ok {
+		return fmt.Errorf("failed to append cert to cert pool!")
+	}
+
+	return mysql.RegisterTLSConfig("rds", &tls.Config{RootCAs: rootCertPool, InsecureSkipVerify: true})
 }
