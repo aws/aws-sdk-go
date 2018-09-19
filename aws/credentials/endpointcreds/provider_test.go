1b677c322720d132bba5fa6ef257bafc18135df7
Jason Del Ponte <delpontej@gmail.com>
2018-09-18 17:00:06 -0700

    aws/default: Add Authorization header for endpoint credential provider
    Updates the endpoint credential provider support for specifing an
    AuthorizationToken value that will be used as the `Authorization` HTTP
    header.
    
    Add support for the AWS_CONTAINER_AUTHORIZATION_TOKEN environment
    variable to specify the Authorization token for ECS Container
    and generic HTTP credential providers.
    
    This fix allows applications written with the Go SDK to retrieve
    credentials while running within the AWS Greengrass platform.

--- aws/credentials/endpointcreds/provider_test.go
+++ aws/credentials/endpointcreds/provider_test.go
@@ -154,3 +154,65 @@ func TestFailedRetrieveCredentials(t *testing.T) {
 		t.Errorf("expect expired, wasn't")
 	}
 }
+
+func TestAuthorizationToken(t *testing.T) {
+	const expectAuthToken = "Basic abc123"
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if e, a := "/path/to/endpoint", r.URL.Path; e != a {
+			t.Errorf("expect %v, got %v", e, a)
+		}
+		if e, a := "application/json", r.Header.Get("Accept"); e != a {
+			t.Errorf("expect %v, got %v", e, a)
+		}
+		if e, a := expectAuthToken, r.Header.Get("Authorization"); e != a {
+			t.Fatalf("expect %v, got %v", e, a)
+		}
+
+		encoder := json.NewEncoder(w)
+		err := encoder.Encode(map[string]interface{}{
+			"AccessKeyID":     "AKID",
+			"SecretAccessKey": "SECRET",
+			"Token":           "TOKEN",
+			"Expiration":      time.Now().Add(1 * time.Hour),
+		})
+
+		if err != nil {
+			fmt.Println("failed to write out creds", err)
+		}
+	}))
+
+	client := endpointcreds.NewProviderClient(*unit.Session.Config,
+		unit.Session.Handlers,
+		server.URL+"/path/to/endpoint?something=else",
+		func(p *endpointcreds.Provider) {
+			p.AuthorizationToken = expectAuthToken
+		},
+	)
+	creds, err := client.Retrieve()
+
+	if err != nil {
+		t.Errorf("expect no error, got %v", err)
+	}
+
+	if e, a := "AKID", creds.AccessKeyID; e != a {
+		t.Errorf("expect %v, got %v", e, a)
+	}
+	if e, a := "SECRET", creds.SecretAccessKey; e != a {
+		t.Errorf("expect %v, got %v", e, a)
+	}
+	if e, a := "TOKEN", creds.SessionToken; e != a {
+		t.Errorf("expect %v, got %v", e, a)
+	}
+	if client.IsExpired() {
+		t.Errorf("expect not expired, was")
+	}
+
+	client.(*endpointcreds.Provider).CurrentTime = func() time.Time {
+		return time.Now().Add(2 * time.Hour)
+	}
+
+	if !client.IsExpired() {
+		t.Errorf("expect expired, wasn't")
+	}
+}
