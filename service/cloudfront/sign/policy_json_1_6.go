6b62d2b616e8886c30ecb21bf1e8ad65350100bd
Roger Chapman <roger.chapman@safetyculture.io>
2018-09-27 09:49:09 +1000

    simplify and use core libs

--- service/cloudfront/sign/policy_json_1_6.go
+++ service/cloudfront/sign/policy_json_1_6.go
@@ -5,34 +5,10 @@ package sign
 import (
 	"bytes"
 	"encoding/json"
-	"log"
 )
 
 func encodePolicyJSON(p *Policy) ([]byte, error) {
-	buffer := &bytes.Buffer{}
 	src, err := json.Marshal(p)
-	if err != nil {
-		return nil, err
-	}
-
-	start := 0
-	for i, c := range src {
-		// Convert \u0026 back to &
-		if c == '\\' && i+5 < len(src) && src[i+1] == 'u' && src[i+2] == '0' && src[i+3] == '0' && src[i+4] == '2' && src[i+5] == '6' {
-			if start < i {
-				buffer.Write(src[start:i])
-			}
-			buffer.WriteString("&")
-			start = i + 6
-		}
-		if start < i {
-			buffer.Write(src[start:i])
-			start = start + 1
-		}
-		log.Printf("%+q", c)
-	}
-	if start < len(src) {
-		buffer.Write(src[start:])
-	}
-	return buffer.Bytes(), err
+	// Convert \u0026 back to &
+	return bytes.Replace(src, []byte("\\u0026"), []byte("&"), -1), err
 }
