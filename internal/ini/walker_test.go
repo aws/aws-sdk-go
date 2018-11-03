1c74dc6f44f3aa7d91d1fd423e240220022995d8
xibz <impactbchang@gmail.com>
2018-11-03 13:09:38 -0700

    ensure that numbers that are too large be converted into string types

--- internal/ini/walker_test.go
+++ internal/ini/walker_test.go
@@ -6,54 +6,101 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 )
 
-func TestDataFiles(t *testing.T) {
+func TestValidDataFiles(t *testing.T) {
+	const expectedFileSuffix = "_expected"
+	filepath.Walk("./testdata/valid", func(path string, info os.FileInfo, err error) error {
+		if strings.HasSuffix(path, expectedFileSuffix) {
+			return nil
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		f, err := os.Open(path)
+		if err != nil {
+			t.Errorf("%s: unexpected error, %v", path, err)
+		}
+		defer f.Close()
+
+		tree, err := ParseAST(f)
+		if err != nil {
+			t.Errorf("%s: unexpected parse error, %v", path, err)
+		}
+
+		v := NewDefaultVisitor()
+		err = Walk(tree, v)
+		if err != nil {
+			t.Errorf("%s: unexpected walk error, %v", path, err)
+		}
+
+		expectedPath := path + "_expected"
+		e := map[string]interface{}{}
+
+		b, err := ioutil.ReadFile(expectedPath)
+		if err != nil {
+			// ignore files that do not have an expected file
+			return nil
+		}
+
+		err = json.Unmarshal(b, &e)
+		if err != nil {
+			t.Errorf("unexpected error during deserialization, %v", err)
+		}
+
+		for profile, tableIface := range e {
+			p, ok := v.Sections.GetSection(profile)
+			if !ok {
+				t.Fatal("could not find profile " + profile)
+			}
+
+			table := tableIface.(map[string]interface{})
+			for k, v := range table {
+				switch e := v.(type) {
+				case string:
+					a := p.String(k)
+					if e != a {
+						t.Errorf("%s: expected %v, but received %v", path, e, a)
+					}
+				case int:
+					a := p.Int(k)
+					if int64(e) != a {
+						t.Errorf("%s: expected %v, but received %v", path, e, a)
+					}
+				case float64:
+					v := p.values[k]
+					if v.Type == IntegerType {
+						a := p.Int(k)
+						if int64(e) != a {
+							t.Errorf("%s: expected %v, but received %v", path, e, a)
+						}
+					} else {
+						a := p.Float64(k)
+						if e != a {
+							t.Errorf("%s: expected %v, but received %v", path, e, a)
+						}
+					}
+				default:
+					t.Errorf("unexpected type: %T", e)
+				}
+			}
+		}
+
+		return nil
+	})
+}
+
+func TestInvalidDataFiles(t *testing.T) {
 	cases := []struct {
 		path               string
 		expectedParseError bool
 		expectedWalkError  bool
 	}{
-		{
-			path: "./testdata/valid/empty_profile",
-		},
-		{
-			path: "./testdata/valid/array_profile",
-		},
-		{
-			path: "./testdata/valid/simple_profile",
-		},
-		{
-			path: "./testdata/valid/arn_profile",
-		},
-		{
-			path: "./testdata/valid/commented_profile",
-		},
-		{
-			path: "./testdata/valid/sections_profile",
-		},
-		{
-			path: "./testdata/valid/number_lhs_expr",
-		},
-		{
-			path: "./testdata/valid/base_numbers_profile",
-		},
-		{
-			path: "./testdata/valid/exponent_profile",
-		},
-		{
-			path: "./testdata/valid/escaped_profile",
-		},
-		{
-			path: "./testdata/valid/global_values_profile",
-		},
-		{
-			path: "./testdata/valid/utf_8_profile",
-		},
-		{
-			path: "./testdata/valid/profile_name",
-		},
 		{
 			path:               "./testdata/invalid/bad_syntax_1",
 			expectedParseError: true,
@@ -74,6 +121,7 @@ func TestDataFiles(t *testing.T) {
 			if err != nil {
 				t.Errorf("unexpected error, %v", err)
 			}
+			defer f.Close()
 
 			tree, err := ParseAST(f)
 			if err != nil && !c.expectedParseError {
@@ -88,64 +136,9 @@ func TestDataFiles(t *testing.T) {
 
 			v := NewDefaultVisitor()
 			err = Walk(tree, v)
-			if err != nil && !c.expectedWalkError {
-				t.Errorf("%d: unexpected error, %v", i+1, err)
-			} else if err == nil && c.expectedWalkError {
+			if err == nil && c.expectedWalkError {
 				t.Errorf("%d: expected error, but received none", i+1)
 			}
-
-			expectedPath := c.path + "_expected"
-			e := map[string]interface{}{}
-
-			b, err := ioutil.ReadFile(expectedPath)
-			if err != nil {
-				return
-			}
-
-			err = json.Unmarshal(b, &e)
-			if err != nil {
-				t.Errorf("unexpected error during deserialization, %v", err)
-			}
-
-			for profile, tableIface := range e {
-				p, ok := v.Sections.GetSection(profile)
-				if !ok {
-					t.Fatal("could not find profile " + profile)
-				}
-
-				table := tableIface.(map[string]interface{})
-				for k, v := range table {
-					switch e := v.(type) {
-					case string:
-						a := p.String(k)
-						if e != a {
-							t.Errorf("%d: expected %v, but received %v", i+1, e, a)
-						}
-					case int:
-						a := p.Int(k)
-						if int64(e) != a {
-							t.Errorf("%d: expected %v, but received %v", i+1, e, a)
-						}
-					case float64:
-						v := p.values[k]
-						if v.Type == IntegerType {
-							a := p.Int(k)
-							if int64(e) != a {
-								t.Errorf("%d: expected %v, but received %v", i+1, e, a)
-							}
-						} else {
-							a := p.Float64(k)
-							if e != a {
-								t.Errorf("%d: expected %v, but received %v", i+1, e, a)
-							}
-						}
-					default:
-						t.Errorf("unexpected type: %T", e)
-					}
-				}
-			}
-
-			f.Close()
 		})
 	}
 }
