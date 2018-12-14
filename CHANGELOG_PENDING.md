5f1ca23f3ded773a9ba214e6fac36acd9a965a53
awssdkgo <awssdkgo@awssdkgo-2b-884d864e.us-west-2.amazon.com>
2018-12-14 22:59:53 +0000

    Release v1.16.6

--- CHANGELOG_PENDING.md
+++ CHANGELOG_PENDING.md
@@ -3,6 +3,3 @@
 ### SDK Enhancements
 
 ### SDK Bugs
-* `private/mode/api`: Fix idempotency members not to require validation [#2353](https://github.com/aws/aws-sdk-go/pull/2353)
-  * Fixes the SDK's usage of API operation request members marked as idempotency tokens to not require validation. These fields will be auto populated by the SDK if the user does not provide a value. The SDK was requiring the user to provide a value or disable validation to use these APIs.
-* deps: Update Go Deps lock file to correct tracking hash [#2354](https://github.com/aws/aws-sdk-go/pull/2354)
--- service/alexaforbusiness/doc.go
+++ service/alexaforbusiness/doc.go
@@ -4,15 +4,15 @@
 // requests to Alexa For Business.
 //
 // Alexa for Business helps you use Alexa in your organization. Alexa for Business
-// provides the tools you to manage Alexa devices, enroll your users, and assign
-// skills, at scale. You can build your own context-aware voice skills using
-// the Alexa Skills Kit and the Alexa for Business API operations. You can also
-// make these available as private skills for your organization. Alexa for Business
-// makes it efficient to voice-enable your products and services, thus providing
-// context-aware voice experiences for your customers. In addition, Alexa for
-// Business enables Alexa Voice Services (AVS) device manufacturers to centrally
-// deploy and manage their devices in Alexa for Business as shared devices as
-// a part of their existing management flow.
+// provides you with the tools to manage Alexa devices, enroll your users, and
+// assign skills, at scale. You can build your own context-aware voice skills
+// using the Alexa Skills Kit and the Alexa for Business API operations. You
+// can also make these available as private skills for your organization. Alexa
+// for Business makes it efficient to voice-enable your products and services,
+// thus providing context-aware voice experiences for your customers. Device
+// makers building with the Alexa Voice Service (AVS) can create fully integrated
+// solutions, register their products with Alexa for Business, and manage them
+// as shared devices in their organization.
 //
 // See https://docs.aws.amazon.com/goto/WebAPI/alexaforbusiness-2017-11-09 for more information on this service.
 //
