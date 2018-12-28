708bc46446ebc70e538672c3a18c227e79964a47
Jason Del Ponte <delpontej@gmail.com>
2018-12-28 14:55:12 -0800

    private/model/api: Assert smoke integration tests error are not empty
    Updates SDK integration smoke test code generation to assert that error
    codes are non-empty.
    
    Related to #2377

--- private/model/api/smoke.go
+++ private/model/api/smoke.go
@@ -106,6 +106,9 @@ var smokeTestTmpl = template.Must(template.New(`smokeTestTmpl`).Parse(`
 			if !ok {
 				t.Fatalf("expect awserr, was %T", err)
 			}
+			if len(aerr.Code()) == 0 {
+				t.Errorf("expect non-empty error code")
+			}
 			if v := aerr.Code(); v == request.ErrCodeSerialization {
 				t.Errorf("expect API error code got serialization failure")
 			}
