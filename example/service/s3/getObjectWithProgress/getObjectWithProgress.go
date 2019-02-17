5c87f8e930170d0e243b907dc6e417eb24e54a18
petems <p.morsou+GIT@gmail.com>
2019-02-17 17:19:12 +0000

    Adds example of S3 download with progress
    * Uses io.WriterAt to determine progress
    * Tracks the progress and writes to stdout 
    * Great example for future peoples!

---
+++ example/service/s3/getObjectWithProgress/getObjectWithProgress.go
@@ -0,0 +1,142 @@
+// +build example
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+	"sync/atomic"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
+
+// progressWriter tracks the download progress of a file from S3 to a file
+// as the writeAt method is called, the byte size is added to the written total,
+// and then a log is printed of the written percentage from the total size
+// it looks like this on the command line:
+//  2019/02/22 12:59:15 File size:35943530 downloaded:16360 percentage:0%
+//  2019/02/22 12:59:15 File size:35943530 downloaded:16988 percentage:0%
+//  2019/02/22 12:59:15 File size:35943530 downloaded:33348 percentage:0%
+type progressWriter struct {
+	written int64
+	writer  io.WriterAt
+	size    int64
+}
+
+func (pw *progressWriter) WriteAt(p []byte, off int64) (int, error) {
+
+	atomic.AddInt64(&pw.written, int64(len(p)))
+
+	percentageDownloaded := int(float32(pw.written*100) / float32(pw.size))
+
+	log.Printf("File size:%d downloaded:%d percentage:%d%% \n", pw.size, pw.written, percentageDownloaded)
+
+	return pw.writer.WriteAt(p, off)
+}
+
+func byteCountDecimal(b int64) string {
+	const unit = 1000
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB", float64(b)/float64(div), "kMGTPE"[exp])
+}
+
+func getFileSize(svc *s3.S3, bucket string, prefix string) (filesize int64, error error) {
+	params := &s3.HeadObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(prefix),
+	}
+
+	resp, err := svc.HeadObject(params)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return *resp.ContentLength, nil
+}
+
+func parseFilename(keyString string) (filename string) {
+	ss := strings.Split(keyString, "/")
+	s := ss[len(ss)-1]
+	return s
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Println("USAGE ERROR: AWS_REGION=us-east-1 go run getObjWithProgress.go example-s3-bucket fooapp/foo.zip")
+		return
+	}
+
+	bucket := os.Args[1]
+	key := os.Args[2]
+
+	filename := parseFilename(key)
+
+	sess, err := session.NewSession()
+
+	if err != nil {
+		panic(err)
+	}
+
+	s3Client := s3.New(sess)
+
+	downloader := s3manager.NewDownloader(sess)
+
+	size, err := getFileSize(s3Client, bucket, key)
+
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("File size is: %s", byteCountDecimal(size))
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	temp, err := ioutil.TempFile(cwd, "getObjWithProgress-tmp-")
+
+	tempfileName := temp.Name()
+
+	if err != nil {
+		panic(err)
+	}
+
+	writer := &progressWriter{writer: temp, size: size, written: 0}
+
+	params := &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	}
+
+	if _, err := downloader.Download(writer, params); err != nil {
+		log.Printf("Download failed! Deleting tempfile: %s", tempfileName)
+		os.Remove(tempfileName)
+		panic(err)
+	}
+
+	if err := temp.Close(); err != nil {
+		panic(err)
+	}
+
+	if err := os.Rename(temp.Name(), filename); err != nil {
+		panic(err)
+	}
+
+	log.Printf("File downloaded! Avaliable at: %s", filename)
+}
