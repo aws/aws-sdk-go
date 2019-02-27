e413fe6c393e8363f6ce75915b5c90f2b41f9e9b
Jason Del Ponte <delpontej@gmail.com>
2019-02-26 16:45:49 -0800

    Use correct transport for Go version

--- aws/session/session.go
+++ aws/session/session.go
@@ -6,10 +6,8 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"net"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
@@ -412,18 +410,7 @@ func loadCustomCABundle(s *Session, bundle io.Reader) error {
 		// Nil transport implies `http.DefaultTransport` should be used. Since
 		// the SDK cannot modify, nor copy the `DefaultTransport` specifying
 		// the values the next closest behavior.
-		t = &http.Transport{
-			Proxy: http.ProxyFromEnvironment,
-			DialContext: (&net.Dialer{
-				Timeout:   30 * time.Second,
-				KeepAlive: 30 * time.Second,
-				DualStack: true,
-			}).DialContext,
-			MaxIdleConns:          100,
-			IdleConnTimeout:       90 * time.Second,
-			TLSHandshakeTimeout:   10 * time.Second,
-			ExpectContinueTimeout: 1 * time.Second,
-		}
+		t = getCABundleTransport()
 	}
 
 	p, err := loadCertPool(bundle)
