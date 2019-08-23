3aea156c3637d7c7f49c9f02098b955263353307
yifan-li <yifan.li@gree.net>
2019-08-23 12:05:08 +0900

    service/s3/s3crypto:Add unit test for getWriterStore

--- service/s3/s3crypto/helper_test.go
+++ service/s3/s3crypto/helper_test.go
@@ -2,8 +2,13 @@ package s3crypto
 
 import (
 	"bytes"
+	"net/http"
+	"os"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/awstesting"
 	"github.com/aws/aws-sdk-go/internal/sdkio"
 )
 
@@ -80,5 +85,64 @@ func TestBytesReadWriteSeeker_Seek(t *testing.T) {
 
 	if !bytes.Equal(expected, buf[:n]) {
 		t.Error("expected equivalent byte slices, but received otherwise")
+	}
+}
+
+func TestGetWriterStore_TempFile(t *testing.T) {
+	response := http.Response{StatusCode: 200}
+	s := awstesting.NewClient(aws.NewConfig().WithMaxRetries(10))
+	s.Handlers.Validate.Clear()
+	s.Handlers.Send.Clear() // mock sending
+	s.Handlers.Send.PushBack(func(r *request.Request) {
+		r.HTTPResponse = &response
+	})
+	type testData struct {
+		Data string
+	}
+	out := &testData{}
+	r := s.NewRequest(&request.Operation{Name: "Operation"}, nil, out)
+	f, err := getWriterStore(r, "", true)
+	if err != nil {
+		t.Fatalf("expected no error, but received %v", err)
+	}
+	tempFile, ok := f.(*os.File)
+	if !ok {
+		t.Fatal("io.ReadWriteSeeker expected to be *os.file")
+	}
+	err = r.Send()
+	if err != nil {
+		t.Errorf("expected no error, but received %v", err)
+	}
+	if _, err := os.Stat(tempFile.Name()); !os.IsNotExist(err) {
+		t.Errorf("expected temp file be deleted, but still exists %v", tempFile.Name())
+	}
+}
+
+func TestGetWriterStore_Memory(t *testing.T) {
+	response := http.Response{StatusCode: 200}
+	s := awstesting.NewClient(aws.NewConfig().WithMaxRetries(10))
+	s.Handlers.Validate.Clear()
+	s.Handlers.Send.Clear() // mock sending
+	s.Handlers.Send.PushBack(func(r *request.Request) {
+		r.HTTPResponse = &response
+	})
+	type testData struct {
+		Data string
+	}
+	out := &testData{}
+	r := s.NewRequest(&request.Operation{Name: "Operation"}, nil, out)
+	f, err := getWriterStore(r, "", false)
+	if err != nil {
+		t.Fatalf("expected no error, but received %v", err)
+	}
+	if _, ok := f.(*bytesReadWriteSeeker); !ok {
+		t.Fatal("io.ReadWriteSeeker expected to be *bytesReadWriteSeeker")
+	}
+	if err != nil {
+		t.Errorf("expected no error, but received %v", err)
+	}
+	err = r.Send()
+	if err != nil {
+		t.Errorf("expected no error, but received %v", err)
 	}
 }
