2f49124a8a5386cbea9b1350f6b9c881b9489c6f
Sharath Patlolla <spatlolla@umass.edu>
2019-08-28 15:16:05 -0700

    private/model/api: Add suport for additional types to ShapeValueBuilder (#2762)
    Update SDK's ShapeValueBuilder code generation utility to support timestamp input values of different formats depending on source, e.g. example and smoke tests. 
    
    Also updates ShapeValueBuilder to support blob API types input tests with base64 or string values.

--- private/model/api/shape_value_builder.go
+++ private/model/api/shape_value_builder.go
@@ -3,14 +3,35 @@
 package api
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"reflect"
 	"sort"
 	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/private/protocol"
 )
 
 // ShapeValueBuilder provides the logic to build the nested values for a shape.
-type ShapeValueBuilder struct{}
+// Base64BlobValues is true if the blob field in shapeRef.Shape.Type is base64
+// encoded.
+type ShapeValueBuilder struct {
+	// Specifies if API shapes modeled as blob types, input values are base64
+	// encoded or not, and strings values instead.
+	Base64BlobValues bool
+
+	// The helper that will provide the logic and formated code to convert a
+	// timestamp input value into a Go time.Time.
+	ParseTimeString func(ref *ShapeRef, memberName, v string) string
+}
+
+// NewShapeValueBuilder returns an initialized ShapeValueBuilder for generating
+// API shape types initialized with values.
+func NewShapeValueBuilder() ShapeValueBuilder {
+	return ShapeValueBuilder{ParseTimeString: parseUnixTimeString}
+}
 
 // BuildShape will recursively build the referenced shape based on the json
 // object provided.  isMap will dictate how the field name is specified. If
@@ -37,18 +58,17 @@ func (b ShapeValueBuilder) BuildShape(ref *ShapeRef, shapes map[string]interface
 
 		memName := name
 		passRef := ref.Shape.MemberRefs[name]
-
 		if isMap {
 			memName = fmt.Sprintf("%q", memName)
 			passRef = &ref.Shape.ValueRef
 		}
-
 		switch v := shape.(type) {
 		case map[string]interface{}:
-			ret += b.BuildComplex(name, memName, passRef, v)
+			ret += b.BuildComplex(name, memName, passRef, ref.Shape, v)
 		case []interface{}:
 			ret += b.BuildList(name, memName, passRef, v)
 		default:
+
 			ret += b.BuildScalar(name, memName, passRef, v, ref.Shape.Payload == name)
 		}
 	}
@@ -128,11 +148,16 @@ func (b ShapeValueBuilder) BuildScalar(name, memName string, ref *ShapeRef, shap
 		return convertToCorrectType(memName, ref.Shape.Type, fmt.Sprintf("%t", v))
 	case int:
 		if ref.Shape.Type == "timestamp" {
-			return parseTimeString(ref, memName, fmt.Sprintf("%d", v))
+			return b.ParseTimeString(ref, memName, fmt.Sprintf("%d", v))
 		}
 		return convertToCorrectType(memName, ref.Shape.Type, fmt.Sprintf("%d", v))
 	case float64:
+
 		dataType := ref.Shape.Type
+
+		if dataType == "timestamp" {
+			return b.ParseTimeString(ref, memName, fmt.Sprintf("%f", v))
+		}
 		if dataType == "integer" || dataType == "int64" || dataType == "long" {
 			return convertToCorrectType(memName, ref.Shape.Type, fmt.Sprintf("%d", int(shape.(float64))))
 		}
@@ -141,12 +166,22 @@ func (b ShapeValueBuilder) BuildScalar(name, memName string, ref *ShapeRef, shap
 		t := ref.Shape.Type
 		switch t {
 		case "timestamp":
-			return parseTimeString(ref, memName, fmt.Sprintf("%s", v))
+			return b.ParseTimeString(ref, memName, fmt.Sprintf("%s", v))
+
+		case "jsonvalue":
+			return fmt.Sprintf("%s: %#v,\n", memName, parseJSONString(v))
+
 		case "blob":
 			if (ref.Streaming || ref.Shape.Streaming) && isPayload {
 				return fmt.Sprintf("%s: aws.ReadSeekCloser(strings.NewReader(%q)),\n", memName, v)
 			}
-
+			if b.Base64BlobValues {
+				decodedBlob, err := base64.StdEncoding.DecodeString(v)
+				if err != nil {
+					panic(fmt.Errorf("Failed to decode string: %v", err))
+				}
+				return fmt.Sprintf("%s: []byte(%q),\n", memName, decodedBlob)
+			}
 			return fmt.Sprintf("%s: []byte(%q),\n", memName, v)
 		default:
 			return convertToCorrectType(memName, t, v)
@@ -158,18 +193,32 @@ func (b ShapeValueBuilder) BuildScalar(name, memName string, ref *ShapeRef, shap
 
 // BuildComplex will build the shape's value for complex types such as structs,
 // and maps.
-func (b ShapeValueBuilder) BuildComplex(name, memName string, ref *ShapeRef, v map[string]interface{}) string {
-	switch ref.Shape.Type {
+func (b ShapeValueBuilder) BuildComplex(name, memName string, ref *ShapeRef, parent *Shape, v map[string]interface{}) string {
+	switch parent.Type {
 	case "structure":
-		return fmt.Sprintf(`%s: &%s{
+		if ref.Shape.Type == "map" {
+			return fmt.Sprintf(`%s: %s{
+				%s
+			},
+			`, memName, b.GoType(ref, true), b.BuildShape(ref, v, true))
+		} else {
+			return fmt.Sprintf(`%s: &%s{
 				%s
 			},
 			`, memName, b.GoType(ref, true), b.BuildShape(ref, v, false))
+		}
 	case "map":
-		return fmt.Sprintf(`%s: %s{
+		if ref.Shape.Type == "map" {
+			return fmt.Sprintf(`%q: %s{
 				%s
 			},
 			`, name, b.GoType(ref, false), b.BuildShape(ref, v, true))
+		} else {
+			return fmt.Sprintf(`%s: &%s{
+				%s
+			},
+			`, memName, b.GoType(ref, true), b.BuildShape(ref, v, false))
+		}
 	default:
 		panic(fmt.Sprintf("Expected complex type but received %q", ref.Shape.Type))
 	}
@@ -177,6 +226,7 @@ func (b ShapeValueBuilder) BuildComplex(name, memName string, ref *ShapeRef, v m
 
 // GoType returns the string of the shape's Go type identifier.
 func (b ShapeValueBuilder) GoType(ref *ShapeRef, elem bool) string {
+
 	if ref.Shape.Type != "structure" && ref.Shape.Type != "list" && ref.Shape.Type != "map" {
 		// Scalars are always pointers.
 		return ref.GoTypeWithPkgName()
@@ -192,4 +242,35 @@ func (b ShapeValueBuilder) GoType(ref *ShapeRef, elem bool) string {
 		return prefix + ref.Shape.GoTypeWithPkgNameElem()
 	}
 	return prefix + ref.GoTypeWithPkgName()
+}
+
+// parseJSONString a json string and returns aws.JSONValue.
+func parseJSONString(input string) aws.JSONValue {
+	var v aws.JSONValue
+	if err := json.Unmarshal([]byte(input), &v); err != nil {
+		panic(fmt.Sprintf("unable to unmarshal JSONValue, %v", err))
+	}
+	return v
+}
+
+// InlineParseModeledTime returns the string of an inline function which
+// returns time.
+func inlineParseModeledTime(format, v string) string {
+	const formatTimeTmpl = `func() *time.Time{
+        v, err := protocol.ParseTime("%s", "%s")
+        if err != nil {
+            panic(err)
+        }
+        return &v
+    }()`
+
+	return fmt.Sprintf(formatTimeTmpl, format, v)
+}
+
+// parseUnixTimeString returns a string which assigns the value of a time
+// member using an inline function Defined inline function parses time in
+// UnixTimeFormat.
+func parseUnixTimeString(ref *ShapeRef, memName, v string) string {
+	ref.API.AddSDKImport("private/protocol")
+	return fmt.Sprintf("%s: %s,\n", memName, inlineParseModeledTime(protocol.UnixTimeFormatName, v))
 }
