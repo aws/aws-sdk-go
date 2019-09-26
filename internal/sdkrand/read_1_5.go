a2ef28dfb9ca8d1c6819ce77560173dd39b2e2ad
Jason Del Ponte <delpontej@gmail.com>
2019-09-26 14:38:06 -0700

    fix go1.5 build issue

--- internal/sdkrand/read_1_5.go
+++ internal/sdkrand/read_1_5.go
@@ -5,11 +5,11 @@ package sdkrand
 import "math/rand"
 
 // Read backfills Go 1.6's math.Rand.Reader for Go 1.5
-func Read(r *rand.Rand, p []byte) (int, error) {
+func Read(r *rand.Rand, p []byte) (n int, err error) {
 	// Copy of Go standard libraries math package's read function not added to
 	// standard library until Go 1.6.
-	var pos int64
-	var val int8
+	var pos int8
+	var val int64
 	for n = 0; n < len(p); n++ {
 		if pos == 0 {
 			val = r.Int63()
@@ -19,5 +19,6 @@ func Read(r *rand.Rand, p []byte) (int, error) {
 		val >>= 8
 		pos--
 	}
-	return
+
+	return n, err
 }
