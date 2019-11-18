c2cafebe5b990a9ed8f1566ed983102c700458ee
Sean McGrail <mcgrails@amazon.com>
2019-11-18 10:50:39 -0800

    private/model/api: Show how to create a session in client examples

--- private/model/api/api.go
+++ private/model/api/api.go
@@ -517,6 +517,8 @@ const (
 // aws.Config parameter to add your extra config.
 //
 // Example:
+//     mySession := session.Must(session.NewSession())
+//
 //     // Create a {{ .StructName }} client from just a session.
 //     svc := {{ .PackageName }}.New(mySession)
 //
