6855e8a14c7beca9ede720cfb27d38087373a8cc
Sean McGrail <mcgrails@amazon.com>
2019-12-12 09:57:23 -0800

    private/protocol/eventstream/eventstreamapi: Add MessageSigner to EventWriter (#3016)

--- private/protocol/eventstream/eventstreamapi/writer_test.go
+++ private/protocol/eventstream/eventstreamapi/writer_test.go
@@ -1,9 +1,13 @@
+// +build go1.7
+
 package eventstreamapi
 
 import (
 	"bytes"
+	"encoding/hex"
 	"reflect"
 	"testing"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/request"
@@ -14,8 +18,10 @@ import (
 
 func TestEventWriter(t *testing.T) {
 	cases := map[string]struct {
-		Event  Marshaler
-		Expect eventstream.Message
+		Event    Marshaler
+		Signer   *MessageSigner
+		TimeFunc func() time.Time
+		Expect   eventstream.Message
 	}{
 		"structured event": {
 			Event: &eventStructured{
@@ -37,23 +43,61 @@ func TestEventWriter(t *testing.T) {
 				Payload: []byte(`{"String":"stringfield","Number":123,"Nested":{"String":"fieldstring","Number":321}}`),
 			},
 		},
+		"signed event": {
+			Event: &eventStructured{
+				String: aws.String("stringfield"),
+				Number: aws.Int64(123),
+				Nested: &eventStructured{
+					String: aws.String("fieldstring"),
+					Number: aws.Int64(321),
+				},
+			},
+			Signer: &MessageSigner{Signer: &mockChunkSigner{signature: "524f1d03d1d81e94a099042736d40bd9681b867321443ff58a4568e274dbd83bff"}},
+			TimeFunc: func() time.Time {
+				return time.Date(2019, 1, 27, 22, 37, 54, 0, time.UTC)
+			},
+			Expect: eventstream.Message{
+				Headers: eventstream.Headers{
+					eventMessageTypeHeader,
+					eventstream.Header{
+						Name:  EventTypeHeader,
+						Value: eventstream.StringValue("eventStructured"),
+					},
+					{
+						Name:  DateHeader,
+						Value: eventstream.TimestampValue(time.Date(2019, 1, 27, 22, 37, 54, 0, time.UTC)),
+					},
+					{
+						Name:  ChunkSignatureHeader,
+						Value: eventstream.BytesValue(mustDecodeHex(hex.DecodeString("524f1d03d1d81e94a099042736d40bd9681b867321443ff58a4568e274dbd83bff"))),
+					},
+				},
+				Payload: []byte(`{"String":"stringfield","Number":123,"Nested":{"String":"fieldstring","Number":321}}`),
+			},
+		},
 	}
 
 	var marshalers request.HandlerList
 	marshalers.PushBackNamed(restjson.BuildHandler)
 
 	var stream bytes.Buffer
-	eventWriter := NewEventWriter(&stream,
-		protocol.HandlerPayloadMarshal{
-			Marshalers: marshalers,
-		},
-	)
-
-	decoder := eventstream.NewDecoder(&stream)
 
 	decodeBuf := make([]byte, 1024)
 	for name, c := range cases {
 		t.Run(name, func(t *testing.T) {
+			defer swapTimeNow(c.TimeFunc)()
+
+			stream.Reset()
+
+			eventWriter := NewEventWriter(&stream,
+				protocol.HandlerPayloadMarshal{
+					Marshalers: marshalers,
+				},
+				c.Signer,
+			)
+
+			decoder := eventstream.NewDecoder(&stream)
+
 			eventWriter.UseLogger(t, aws.LogDebugWithEventStreamBody)
 
 			if err := eventWriter.WriteEvent(c.Event); err != nil {
@@ -81,6 +125,7 @@ func BenchmarkEventWriter(b *testing.B) {
 		protocol.HandlerPayloadMarshal{
 			Marshalers: marshalers,
 		},
+		nil,
 	)
 
 	event := &eventStructured{
@@ -98,31 +143,4 @@ func BenchmarkEventWriter(b *testing.B) {
 			b.Fatalf("expect no write error, got %v", err)
 		}
 	}
-}
-
-type eventStructured struct {
-	_ struct{} `type:"structure"`
-
-	String *string          `type:"string"`
-	Number *int64           `type:"long"`
-	Nested *eventStructured `type:"structure"`
-}
-
-func (e *eventStructured) MarshalEvent(pm protocol.PayloadMarshaler) (eventstream.Message, error) {
-	var msg eventstream.Message
-	msg.Headers.Set(MessageTypeHeader, eventstream.StringValue(EventMessageType))
-	msg.Headers.Set(EventTypeHeader, eventstream.StringValue("eventStructured"))
-
-	var buf bytes.Buffer
-	if err := pm.MarshalPayload(&buf, e); err != nil {
-		return eventstream.Message{}, err
-	}
-
-	msg.Payload = buf.Bytes()
-
-	return msg, nil
-}
-
-func (e *eventStructured) UnmarshalEvent(pm protocol.PayloadUnmarshaler, msg eventstream.Message) error {
-	return pm.UnmarshalPayload(bytes.NewReader(msg.Payload), e)
 }
