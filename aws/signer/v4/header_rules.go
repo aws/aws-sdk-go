f955d303c50d6533e4c9f2dd159fdf493812bb6f
Sean McGrail <mcgrails@amazon.com>
2019-12-19 11:05:36 -0800

    aws/signer/v4: Update to use awsutil.StringHasPrefixFold

--- aws/signer/v4/header_rules.go
+++ aws/signer/v4/header_rules.go
@@ -1,8 +1,7 @@
 package v4
 
 import (
-	"net/http"
-	"strings"
+	"github.com/aws/aws-sdk-go/aws/awsutil"
 )
 
 // validator houses a set of rule needed for validation of a
@@ -61,7 +60,7 @@ type patterns []string
 // been found
 func (p patterns) IsValid(value string) bool {
 	for _, pattern := range p {
-		if strings.HasPrefix(http.CanonicalHeaderKey(value), pattern) {
+		if awsutil.StringHasPrefixFold(value, pattern) {
 			return true
 		}
 	}
