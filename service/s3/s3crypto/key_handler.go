7b954613c2a24e66b935dacd8e8257ddf3b50119
Jason Del Ponte <delpontej@gmail.com>
2020-01-02 12:25:35 -0800

    add support for ctx to kms for both encrypt and decrypt

--- service/s3/s3crypto/key_handler.go
+++ service/s3/s3crypto/key_handler.go
@@ -13,6 +13,13 @@ type CipherDataGenerator interface {
 	GenerateCipherData(int, int) (CipherData, error)
 }
 
+// CipherDataGeneratorWithContext handles generating proper key and IVs of
+// proper size for the content cipher. CipherDataGenerator will also encrypt
+// the key and store it in the CipherData.
+type CipherDataGeneratorWithContext interface {
+	GenerateCipherDataWithContext(aws.Context, int, int) (CipherData, error)
+}
+
 // CipherDataDecrypter is a handler to decrypt keys from the envelope.
 type CipherDataDecrypter interface {
 	DecryptKey([]byte) ([]byte, error)
