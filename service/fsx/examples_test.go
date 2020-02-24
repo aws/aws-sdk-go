bf7b8100224017f0d58b6d24c0bc501aac464d7c
awssdkgo <awssdkgo@awssdkgo-2b-a8624b5c.us-west-2.amazon.com>
2020-02-24 19:24:59 +0000

    Release v1.29.9 (2020-02-24) ===
    ### Service Client Updates
    * `service/docdb`: Updates service documentation
      * Documentation updates for docdb
    * `service/eventbridge`: Updates service API and documentation
    * `service/events`: Updates service API and documentation
      * This release allows you to create and manage tags for event buses.
    * `service/fsx`: Updates service API and documentation
    * `service/iotevents`: Updates service documentation
    * `service/snowball`: Updates service API and documentation
      * AWS Snowball adds a field for entering your GSTIN when creating AWS Snowball jobs in the Asia Pacific (Mumbai) region.

--- service/fsx/examples_test.go
+++ service/fsx/examples_test.go
@@ -119,6 +119,8 @@ func ExampleFSx_CreateFileSystem_shared00() {
 				fmt.Println(fsx.ErrCodeInvalidExportPath, aerr.Error())
 			case fsx.ErrCodeInvalidNetworkSettings:
 				fmt.Println(fsx.ErrCodeInvalidNetworkSettings, aerr.Error())
+			case fsx.ErrCodeInvalidPerUnitStorageThroughput:
+				fmt.Println(fsx.ErrCodeInvalidPerUnitStorageThroughput, aerr.Error())
 			case fsx.ErrCodeServiceLimitExceeded:
 				fmt.Println(fsx.ErrCodeServiceLimitExceeded, aerr.Error())
 			case fsx.ErrCodeInternalServerError:
