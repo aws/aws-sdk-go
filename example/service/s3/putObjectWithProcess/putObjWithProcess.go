b90014aa7827437b354c0ac3bc19020196be81d7
mayongze <1014057907@qq.com>
2020-06-16 15:48:00 +0800

    fix example for file upload with progress

--- example/service/s3/putObjectWithProcess/putObjWithProcess.go
+++ example/service/s3/putObjectWithProcess/putObjWithProcess.go
@@ -3,20 +3,21 @@
 package main
 
 import (
-	"log"
-	"os"
-	"sync/atomic"
-
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"log"
+	"os"
+	"sync"
 )
 
 type CustomReader struct {
-	fp   *os.File
-	size int64
-	read int64
+	fp      *os.File
+	size    int64
+	read    int64
+	signMap map[int64]struct{}
+	mux     sync.Mutex
 }
 
 func (r *CustomReader) Read(p []byte) (int, error) {
@@ -29,14 +30,16 @@ func (r *CustomReader) ReadAt(p []byte, off int64) (int, error) {
 		return n, err
 	}
 
-	// Got the length have read( or means has uploaded), and you can construct your message
-	atomic.AddInt64(&r.read, int64(n))
-
-	// I have no idea why the read length need to be div 2,
-	// maybe the request read once when Sign and actually send call ReadAt again
-	// It works for me
-	log.Printf("total read:%d    progress:%d%%\n", r.read/2, int(float32(r.read*100/2)/float32(r.size)))
-
+	r.mux.Lock()
+	// Ignore the first signature call
+	if _, ok := r.signMap[off]; ok {
+		// Got the length have read( or means has uploaded), and you can construct your message
+		r.read += int64(n)
+		log.Printf("total read:%d    progress:%d%%\n", r.read, int(float32(r.read*100)/float32(r.size)))
+	} else {
+		r.signMap[off] = struct{}{}
+	}
+	r.mux.Unlock()
 	return n, err
 }
 
@@ -78,8 +81,9 @@ func main() {
 	}
 
 	reader := &CustomReader{
-		fp:   file,
-		size: fileInfo.Size(),
+		fp:      file,
+		size:    fileInfo.Size(),
+		signMap: map[int64]struct{}{},
 	}
 
 	uploader := s3manager.NewUploader(sess, func(u *s3manager.Uploader) {
