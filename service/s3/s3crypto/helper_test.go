698e6f09758bf694a049dee36b0655b5219181c8
Gareth Watts <gareth@omnipotent.net>
2020-07-24 11:38:58 -0500

    service/s3/s3crypto Fix request retries (#3344)

--- service/s3/s3crypto/helper_test.go
+++ service/s3/s3crypto/helper_test.go
@@ -2,6 +2,7 @@ package s3crypto
 
 import (
 	"bytes"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"testing"
@@ -115,6 +116,43 @@ func TestGetWriterStore_TempFile(t *testing.T) {
 	}
 	if _, err := os.Stat(tempFile.Name()); !os.IsNotExist(err) {
 		t.Errorf("expected temp file be deleted, but still exists %v", tempFile.Name())
+	}
+}
+
+func TestGetWriterStore_TempFileWithRetry(t *testing.T) {
+	responses := []*http.Response{
+		{StatusCode: 500, Header: http.Header{}, Body: ioutil.NopCloser(&bytes.Buffer{})},
+		{StatusCode: 200, Header: http.Header{}, Body: ioutil.NopCloser(&bytes.Buffer{})},
+	}
+	s := awstesting.NewClient(aws.NewConfig().WithMaxRetries(10))
+	s.Handlers.Validate.Clear()
+	s.Handlers.Send.Clear() // mock sending
+	s.Handlers.Send.PushBack(func(r *request.Request) {
+		r.HTTPResponse = responses[0]
+		responses = responses[1:]
+	})
+	type testData struct {
+		Data string
+	}
+	out := &testData{}
+	r := s.NewRequest(&request.Operation{Name: "Operation"}, nil, out)
+	f, err := getWriterStore(r, "", true)
+	if err != nil {
+		t.Fatalf("expected no error, but received %v", err)
+	}
+	tempFile, ok := f.(*os.File)
+	if !ok {
+		t.Fatal("io.ReadWriteSeeker expected to be *os.file")
+	}
+	err = r.Send()
+	if err != nil {
+		t.Errorf("expected no error, but received %v", err)
+	}
+	if _, err := os.Stat(tempFile.Name()); !os.IsNotExist(err) {
+		t.Errorf("expected temp file be deleted, but still exists %v", tempFile.Name())
+	}
+	if v := len(responses); v != 0 {
+		t.Errorf("expect all retries to be used, have %v remaining", v)
 	}
 }
 
