7a5448855eeb2ef6e411e0117076107eddd2ec21
Jason Del Ponte <delpontej@gmail.com>
2020-08-10 10:22:46 -0700

    add comment for reason for naming scheme.

--- private/model/api/shape.go
+++ private/model/api/shape.go
@@ -863,6 +863,7 @@ const (
 	{{ end }}
 )
 
+{{/* Enum iterators use non-idomatic _Values suffix to avoid naming collisions with other generated types, and enum values */}}
 // {{ $.ShapeName }}_Values returns all elements of the {{ $.ShapeName }} enum
 func {{ $.ShapeName }}_Values() []string {
 	return []string{
