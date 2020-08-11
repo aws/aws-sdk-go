9d7f162dc499f50872f24b83e0c66a5aff944788
Sean McGrail <mcgrails@amazon.com>
2020-08-11 12:50:28 -0700

    Improvements to parsing float representations of time.

--- private/protocol/json/jsonutil/unmarshal.go
+++ private/protocol/json/jsonutil/unmarshal.go
@@ -6,7 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"math"
+	"math/big"
 	"reflect"
 	"strings"
 	"time"
@@ -15,6 +15,8 @@ import (
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/private/protocol"
 )
+
+var millisecondsFloat = new(big.Float).SetInt64(1e3)
 
 // UnmarshalJSONError unmarshal's the reader's JSON document into the passed in
 // type. The value to unmarshal the json document into must be a pointer to the
@@ -40,7 +42,9 @@ func UnmarshalJSONError(v interface{}, stream io.Reader) error {
 func UnmarshalJSON(v interface{}, stream io.Reader) error {
 	var out interface{}
 
-	err := json.NewDecoder(stream).Decode(&out)
+	decoder := json.NewDecoder(stream)
+	decoder.UseNumber()
+	err := decoder.Decode(&out)
 	if err == io.EOF {
 		return nil
 	} else if err != nil {
@@ -55,7 +59,9 @@ func UnmarshalJSON(v interface{}, stream io.Reader) error {
 func UnmarshalJSONCaseInsensitive(v interface{}, stream io.Reader) error {
 	var out interface{}
 
-	err := json.NewDecoder(stream).Decode(&out)
+	decoder := json.NewDecoder(stream)
+	decoder.UseNumber()
+	err := decoder.Decode(&out)
 	if err == io.EOF {
 		return nil
 	} else if err != nil {
@@ -255,18 +261,31 @@ func (u unmarshaler) unmarshalScalar(value reflect.Value, data interface{}, tag 
 		default:
 			return fmt.Errorf("unsupported value: %v (%s)", value.Interface(), value.Type())
 		}
-	case float64:
+	case json.Number:
 		switch value.Interface().(type) {
 		case *int64:
-			di := int64(d)
+			// Retain the old behavior where we would just truncate the float64
+			// calling d.Int64() here would cause an invalid sytnax error due to the usage of strconv.ParseInt
+			f, err := d.Float64()
+			if err != nil {
+				return err
+			}
+			di := int64(f)
 			value.Set(reflect.ValueOf(&di))
 		case *float64:
-			value.Set(reflect.ValueOf(&d))
+			f, err := d.Float64()
+			if err != nil {
+				return err
+			}
+			value.Set(reflect.ValueOf(&f))
 		case *time.Time:
-			// Time unmarshaled from a float64 can only be epoch seconds
-			seconds, frac := math.Modf(d)
-			ms := math.Round(frac * math.Pow10(3)) // Convert fraction to milliseconds and the round the value
-			t := time.Unix(int64(seconds), int64(ms*math.Pow10(6))).UTC()
+			float, ok := new(big.Float).SetString(d.String())
+			if !ok {
+				return fmt.Errorf("unsupported time value: %v (%s)", value.Interface(), value.Type())
+			}
+			float = float.Mul(float, millisecondsFloat)
+			ns, _ := float.Int64()
+			t := time.Unix(0, ns*1e6).UTC()
 			value.Set(reflect.ValueOf(&t))
 		default:
 			return fmt.Errorf("unsupported value: %v (%s)", value.Interface(), value.Type())
--- private/protocol/json/jsonutil/unmarshal_test.go
+++ private/protocol/json/jsonutil/unmarshal_test.go
@@ -8,12 +8,15 @@ import (
 	"testing"
 	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/private/protocol/json/jsonutil"
 )
 
-func TestUnmarshalJSON_Time(t *testing.T) {
+func TestUnmarshalJSON_JSONNumber(t *testing.T) {
 	type input struct {
-		TimeField *time.Time `locationName:"timeField"`
+		TimeField  *time.Time `locationName:"timeField"`
+		IntField   *int64     `locationName:"intField"`
+		FloatField *float64   `locationName:"floatField"`
 	}
 
 	cases := map[string]struct {
@@ -39,8 +42,8 @@ func TestUnmarshalJSON_Time(t *testing.T) {
 				}(),
 			},
 		},
-		"milliseconds precision round down": {
-			JSON: `{"timeField":1597094942.1234}`,
+		"microsecond precision truncated": {
+			JSON: `{"timeField":1597094942.1235}`,
 			Expected: input{
 				TimeField: func() *time.Time {
 					dt := time.Date(2020, 8, 10, 21, 29, 02, int(123*time.Millisecond), time.UTC)
@@ -48,13 +51,31 @@ func TestUnmarshalJSON_Time(t *testing.T) {
 				}(),
 			},
 		},
-		"milliseconds precision round up": {
-			JSON: `{"timeField":1597094942.1235}`,
+		"nanosecond precision truncated": {
+			JSON: `{"timeField":1597094942.123456789}`,
 			Expected: input{
 				TimeField: func() *time.Time {
-					dt := time.Date(2020, 8, 10, 21, 29, 02, int(124*time.Millisecond), time.UTC)
+					dt := time.Date(2020, 8, 10, 21, 29, 02, int(123*time.Millisecond), time.UTC)
 					return &dt
 				}(),
+			},
+		},
+		"integer field": {
+			JSON: `{"intField":123456789}`,
+			Expected: input{
+				IntField: aws.Int64(123456789),
+			},
+		},
+		"integer field truncated": {
+			JSON: `{"intField":123456789.123}`,
+			Expected: input{
+				IntField: aws.Int64(123456789),
+			},
+		},
+		"float64 field": {
+			JSON: `{"floatField":123456789.123}`,
+			Expected: input{
+				FloatField: aws.Float64(123456789.123),
 			},
 		},
 	}
