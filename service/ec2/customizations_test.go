10a6e5a727826062dde099187b66c7018f3705f3
Jason Del Ponte <delpontej@gmail.com>
2020-08-12 10:44:36 -0700

    service/ec2: Fix max retries with client customizations (#3465)
    Fixes #3374 by correcting the EC2 API client's customization for ModifyNetworkInterfaceAttribute and AssignPrivateIpAddresses operations to use the aws.Config.MaxRetries value if set. Previously the API client's customizations would ignore MaxRetries specified in the SDK's aws.Config.MaxRetries field.

--- service/ec2/customizations_test.go
+++ service/ec2/customizations_test.go
@@ -1,12 +1,19 @@
+// +build go1.7
+
 package ec2_test
 
 import (
+	"bytes"
+	"context"
 	"io/ioutil"
+	"net/http"
 	"net/url"
 	"regexp"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
+	sdkclient "github.com/aws/aws-sdk-go/aws/client"
+	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/awstesting/unit"
 	"github.com/aws/aws-sdk-go/service/ec2"
 )
@@ -44,5 +51,57 @@ func TestCopySnapshotPresignedURL(t *testing.T) {
 	r := regexp.MustCompile(`^https://ec2\.us-west-1\.amazonaws\.com/.+&DestinationRegion=us-west-2`)
 	if !r.MatchString(u) {
 		t.Errorf("expect %v to match, got %v", r.String(), u)
+	}
+}
+
+func TestNoCustomRetryerWithMaxRetries(t *testing.T) {
+	cases := map[string]struct {
+		Config           aws.Config
+		ExpectMaxRetries int
+	}{
+		"With custom retrier": {
+			Config: aws.Config{
+				Retryer: sdkclient.DefaultRetryer{
+					NumMaxRetries: 10,
+				},
+			},
+			ExpectMaxRetries: 10,
+		},
+		"with max retries": {
+			Config: aws.Config{
+				MaxRetries: aws.Int(10),
+			},
+			ExpectMaxRetries: 10,
+		},
+		"no options set": {
+			ExpectMaxRetries: sdkclient.DefaultRetryerMaxNumRetries,
+		},
+	}
+
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			client := ec2.New(unit.Session, &aws.Config{
+				DisableParamValidation: aws.Bool(true),
+			}, c.Config.Copy())
+			client.ModifyNetworkInterfaceAttributeWithContext(context.Background(), nil, checkRetryerMaxRetries(t, c.ExpectMaxRetries))
+			client.AssignPrivateIpAddressesWithContext(context.Background(), nil, checkRetryerMaxRetries(t, c.ExpectMaxRetries))
+		})
+	}
+
+}
+
+func checkRetryerMaxRetries(t *testing.T, maxRetries int) func(*request.Request) {
+	return func(r *request.Request) {
+		r.Handlers.Send.Clear()
+		r.Handlers.Send.PushBack(func(rr *request.Request) {
+			if e, a := maxRetries, rr.Retryer.MaxRetries(); e != a {
+				t.Errorf("%s, expect %v max retries, got %v", rr.Operation.Name, e, a)
+			}
+			rr.HTTPResponse = &http.Response{
+				StatusCode: 200,
+				Header:     http.Header{},
+				Body:       ioutil.NopCloser(&bytes.Buffer{}),
+			}
+		})
 	}
 }
