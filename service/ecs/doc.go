4f2adfbb528a9a477904a53a5381bbc174d6322e
awssdkgo <awssdkgo@aws-sdkgo-rel-prod-64001.pdx4.corp.amazon.com>
2021-11-10 19:14:30 +0000

    Release v1.42.2 (2021-11-10) ===
    ### Service Client Updates
    * `service/backup`: Updates service API and documentation
    * `service/ecs`: Updates service API and documentation
      * This release adds support for container instance health.
    * `service/resiliencehub`: Adds new service

--- service/ecs/doc.go
+++ service/ecs/doc.go
@@ -4,21 +4,22 @@
 // requests to Amazon EC2 Container Service.
 //
 // Amazon Elastic Container Service (Amazon ECS) is a highly scalable, fast,
-// container management service that makes it easy to run, stop, and manage
-// Docker containers on a cluster. You can host your cluster on a serverless
-// infrastructure that is managed by Amazon ECS by launching your services or
-// tasks on Fargate. For more control, you can host your tasks on a cluster
-// of Amazon Elastic Compute Cloud (Amazon EC2) instances that you manage.
+// container management service. It makes it easy to run, stop, and manage Docker
+// containers on a cluster. You can host your cluster on a serverless infrastructure
+// that's managed by Amazon ECS by launching your services or tasks on Fargate.
+// For more control, you can host your tasks on a cluster of Amazon Elastic
+// Compute Cloud (Amazon EC2) instances that you manage.
 //
 // Amazon ECS makes it easy to launch and stop container-based applications
-// with simple API calls, allows you to get the state of your cluster from a
-// centralized service, and gives you access to many familiar Amazon EC2 features.
+// with simple API calls. This makes it easy to get the state of your cluster
+// from a centralized service, and gives you access to many familiar Amazon
+// EC2 features.
 //
 // You can use Amazon ECS to schedule the placement of containers across your
 // cluster based on your resource needs, isolation policies, and availability
-// requirements. Amazon ECS eliminates the need for you to operate your own
-// cluster management and configuration management systems or worry about scaling
-// your management infrastructure.
+// requirements. With Amazon ECS, you don't need to operate your own cluster
+// management and configuration management systems. You also don't need to worry
+// about scaling your management infrastructure.
 //
 // See https://docs.aws.amazon.com/goto/WebAPI/ecs-2014-11-13 for more information on this service.
 //
