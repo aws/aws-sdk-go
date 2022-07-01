5cbac95542a2483d14aa2d6624416cb620c9f6b1
Muhammad Abrar <abrar1984@gmail.com>
2022-07-01 15:10:41 +0500

    Golang HTTP server strips off 'Expect' header in which case the server will just assume that its value is 100-continue. The different casings will break signature validation in go based s3 implimentation like minio. so better to just keep it in lower case.

--- private/protocol/eventstream/eventstreamapi/transport_go1.17.go
+++ private/protocol/eventstream/eventstreamapi/transport_go1.17.go
@@ -8,12 +8,12 @@ import "github.com/aws/aws-sdk-go/aws/request"
 // ApplyHTTPTransportFixes applies fixes to the HTTP request for proper event
 // stream functionality. Go 1.15 through 1.17 HTTP client could hang forever
 // when an HTTP/2 connection failed with an non-200 status code and err. Using
-// Expect 100-Continue, allows the HTTP client to gracefully handle the non-200
+// Expect 100-continue, allows the HTTP client to gracefully handle the non-200
 // status code, and close the connection.
 //
 // This is a no-op for Go 1.18 and above.
 func ApplyHTTPTransportFixes(r *request.Request) {
 	r.Handlers.Sign.PushBack(func(r *request.Request) {
-		r.HTTPRequest.Header.Set("Expect", "100-Continue")
+		r.HTTPRequest.Header.Set("Expect", "100-continue")
 	})
 }
