b4def0753fa3ee2e4dbbc636fb1da64dbe5e6569
aws-sdk-go-automation <43143561+aws-sdk-go-automation@users.noreply.github.com>
2022-11-29 12:11:54 -0800

    Release v1.44.148 (2022-11-29) (#4644)
    Release v1.44.148 (2022-11-29)
    ===
    
    ### Service Client Updates
    * `service/ec2`: Updates service API, documentation, and paginators
      * This release adds support for AWS Verified Access and the Hpc6id Amazon EC2 compute optimized instance type, which features 3rd generation Intel Xeon Scalable processors.
      * This release adds support for AWS Verified Access and the Hpc6id Amazon EC2 compute optimized instance type, which features 3rd generation Intel Xeon Scalable processors.
    * `service/firehose`: Updates service API and documentation
      * Allow support for the Serverless offering for Amazon OpenSearch Service as a Kinesis Data Firehose delivery destination.
      * Allow support for the Serverless offering for Amazon OpenSearch Service as a Kinesis Data Firehose delivery destination.
    * `service/kms`: Updates service API, documentation, paginators, and examples
      * AWS KMS introduces the External Key Store (XKS), a new feature for customers who want to protect their data with encryption keys stored in an external key management system under their control.
      * AWS KMS introduces the External Key Store (XKS), a new feature for customers who want to protect their data with encryption keys stored in an external key management system under their control.
    * `service/omics`: Updates service API, documentation, waiters, paginators, and examples
    * `service/opensearchserverless`: Adds new service
    * `service/securitylake`: Adds new service
    * `service/simspaceweaver`: Adds new service

--- service/firehose/doc.go
+++ service/firehose/doc.go
@@ -5,7 +5,8 @@
 //
 // Amazon Kinesis Data Firehose is a fully managed service that delivers real-time
 // streaming data to destinations such as Amazon Simple Storage Service (Amazon
-// S3), Amazon Elasticsearch Service (Amazon ES), Amazon Redshift, and Splunk.
+// S3), Amazon OpenSearch Service, Amazon Redshift, Splunk, and various other
+// supportd destinations.
 //
 // See https://docs.aws.amazon.com/goto/WebAPI/firehose-2015-08-04 for more information on this service.
 //
