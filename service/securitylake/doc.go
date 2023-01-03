2d87e1b7a9b037cb3ad16a0f7ddfc3532b8bd001
aws-sdk-go-automation <43143561+aws-sdk-go-automation@users.noreply.github.com>
2023-01-03 11:16:24 -0800

    Release v1.44.172 (2023-01-03) (#4678)
    Release v1.44.172 (2023-01-03)
    ===
    
    ### Service Client Updates
    * `service/securitylake`: Updates service API and documentation

--- service/securitylake/doc.go
+++ service/securitylake/doc.go
@@ -3,25 +3,43 @@
 // Package securitylake provides the client and types for making API
 // requests to Amazon Security Lake.
 //
-// Amazon Security Lake is in preview release. Your use of the Amazon Security
-// Lake preview is subject to Section 2 of the Amazon Web Services Service Terms
+// Amazon Security Lake is in preview release. Your use of the Security Lake
+// preview is subject to Section 2 of the Amazon Web Services Service Terms
 // (http://aws.amazon.com/service-terms/)("Betas and Previews").
 //
-// Amazon Security Lake is a fully-managed security data lake service. You can
+// Amazon Security Lake is a fully managed security data lake service. You can
 // use Security Lake to automatically centralize security data from cloud, on-premises,
-// and custom sources into a data lake that's stored in your account. Security
-// Lake helps you analyze security data, so you can get a more complete understanding
-// of your security posture across the entire organization and improve the protection
-// of your workloads, applications, and data.
+// and custom sources into a data lake that's stored in your Amazon Web Servicesaccount.
+// Amazon Web Services Organizations is an account management service that lets
+// you consolidate multiple Amazon Web Services accounts into an organization
+// that you create and centrally manage. With Organizations, you can create
+// member accounts and invite existing accounts to join your organization. Security
+// Lake helps you analyze security data for a more complete understanding of
+// your security posture across the entire organization. It can also help you
+// improve the protection of your workloads, applications, and data.
 //
 // The data lake is backed by Amazon Simple Storage Service (Amazon S3) buckets,
 // and you retain ownership over your data.
 //
+// Amazon Security Lake integrates with CloudTrail, a service that provides
+// a record of actions taken by a user, role, or an Amazon Web Services service
+// in Security Lake CloudTrail captures API calls for Security Lake as events.
+// The calls captured include calls from the Security Lake console and code
+// calls to the Security Lake API operations. If you create a trail, you can
+// enable continuous delivery of CloudTrail events to an Amazon S3 bucket, including
+// events for Security Lake. If you don't configure a trail, you can still view
+// the most recent events in the CloudTrail console in Event history. Using
+// the information collected by CloudTrail you can determine the request that
+// was made to Security Lake, the IP address from which the request was made,
+// who made the request, when it was made, and additional details. To learn
+// more about Security Lake information in CloudTrail, see the Amazon Security
+// Lake User Guide (https://docs.aws.amazon.com/security-lake/latest/userguide/securitylake-cloudtrail.html).
+//
 // Security Lake automates the collection of security-related log and event
-// data from integrated Amazon Web Services. and third-party services and manages
-// the lifecycle of data with customizable retention and replication settings.
-// Security Lake also converts ingested data into Apache Parquet format and
-// a standard open-source schema called the Open Cybersecurity Schema Framework
+// data from integrated Amazon Web Services and third-party services. It also
+// helps you manage the lifecycle of data with customizable retention and replication
+// settings. Security Lake converts ingested data into Apache Parquet format
+// and a standard open-source schema called the Open Cybersecurity Schema Framework
 // (OCSF).
 //
 // Other Amazon Web Services and third-party services can subscribe to the data
