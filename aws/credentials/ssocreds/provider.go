2c5d3f314232caf9b2c14421b325365304b3cc8d
Eren Yeager <92114074+wty-Bryant@users.noreply.github.com>
2023-05-31 15:15:35 -0400

    Revert "Add sso token provider"

--- aws/credentials/ssocreds/provider.go
+++ aws/credentials/ssocreds/provider.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha1"
 	"encoding/hex"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"path/filepath"
 	"strings"
@@ -120,6 +121,25 @@ func getCacheFileName(url string) (string, error) {
 		return "", err
 	}
 	return strings.ToLower(hex.EncodeToString(hash.Sum(nil))) + ".json", nil
+}
+
+type rfc3339 time.Time
+
+func (r *rfc3339) UnmarshalJSON(bytes []byte) error {
+	var value string
+
+	if err := json.Unmarshal(bytes, &value); err != nil {
+		return err
+	}
+
+	parse, err := time.Parse(time.RFC3339, value)
+	if err != nil {
+		return fmt.Errorf("expected RFC3339 timestamp: %v", err)
+	}
+
+	*r = rfc3339(parse)
+
+	return nil
 }
 
 type token struct {
