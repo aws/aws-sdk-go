878a5cef35491d73c9041b89f1baad72e9d8f590
Tianyi Wang <wty@amazon.com>
2023-08-17 18:30:59 -0400

    Modify and Merge create token result handle scope

--- aws/credentials/ssocreds/token_provider.go
+++ aws/credentials/ssocreds/token_provider.go
@@ -111,9 +111,14 @@ func (p *SSOTokenProvider) refreshToken(token cachedToken) (cachedToken, error) 
 	if err != nil {
 		return cachedToken{}, fmt.Errorf("unable to refresh SSO token, %v", err)
 	}
-	if createResult == nil || createResult.ExpiresIn == nil ||
-		createResult.AccessToken == nil || createResult.RefreshToken == nil {
-		return cachedToken{}, fmt.Errorf("CreateToken returned some nil val without err")
+	if createResult.ExpiresIn == nil {
+		return cachedToken{}, fmt.Errorf("missing required field ExpiresIn")
+	}
+	if createResult.AccessToken == nil {
+		return cachedToken{}, fmt.Errorf("missing required field AccessToken")
+	}
+	if createResult.RefreshToken == nil {
+		return cachedToken{}, fmt.Errorf("missing required field RefreshToken")
 	}
 
 	expiresAt := nowTime().Add(time.Duration(*createResult.ExpiresIn) * time.Second)
