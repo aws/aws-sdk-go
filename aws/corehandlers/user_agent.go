53ca4dc230f4f23b082d4e54750cbcf15057204a
Isaiah Vita <isvita@amazon.com>
2023-08-29 08:25:05 -0700

    add user agent internal metadata

--- aws/corehandlers/user_agent.go
+++ aws/corehandlers/user_agent.go
@@ -35,3 +35,10 @@ var AddHostExecEnvUserAgentHander = request.NamedHandler{
 		request.AddToUserAgent(r, execEnvUAKey+"/"+v)
 	},
 }
+
+var AddAwsInternal = request.NamedHandler{
+	Name: "core.AddAwsInternal",
+	Fn: func(r *request.Request) {
+		request.AddToUserAgent(r, isAwsInternal)
+	},
+}
